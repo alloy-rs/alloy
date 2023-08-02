d45a19d261373fbada409f00acd2c8e9a32d0824
James <james@prestwi.ch>
2023-08-01 22:36:10 -0700

    cleanup: some clippy and stuff

--- crates/network/src/lib.rs
+++ crates/network/src/lib.rs
@@ -10,42 +10,30 @@ pub trait Transaction: alloy_rlp::Encodable + alloy_rlp::Decodable + RpcObject {
 
 pub trait Eip1559Transaction: Transaction {}
 
-// pub trait Middleware<N: Network>
-// where
-//     <Self::Transport as Transport>::Fut: Send,
-// {
-//     type Transport: Transport;
-//     type Inner: Middleware<N, Transport = Self::Transport>;
+pub trait Middleware<N: Network, T: Transport> {
+    type Inner: Middleware<N, T>;
 
-//     fn client(&self) -> &RpcClient<Self::Transport>;
+    fn client(&self) -> &RpcClient<T>;
 
-//     fn inner(&self) -> &Self::Inner;
+    fn inner(&self) -> &Self::Inner;
 
-//     fn send_transaction(
-//         &self,
-//         tx: &N::Transaction,
-//     ) -> RpcCall<Self::Transport, N::Transaction, N::Receipt> {
-//         self.inner().send_transaction(tx)
-//     }
-// }
+    fn send_transaction(&self, tx: &N::Transaction) -> RpcCall<T, N::Transaction, N::Receipt> {
+        self.inner().send_transaction(tx)
+    }
+}
 
-// impl<N: Network, T: Transport> Middleware<N> for RpcClient<T> {
-//     type Transport = T;
+impl<N: Network, T: Transport> Middleware<N, T> for RpcClient<T> {
+    type Inner = Self;
 
-//     type Inner = Self;
+    fn client(&self) -> &RpcClient<T> {
+        self
+    }
 
-//     fn client(&self) -> &RpcClient<Self::Transport> {
-//         self
-//     }
+    fn inner(&self) -> &Self::Inner {
+        panic!("called inner on RpcClient")
+    }
 
-//     fn inner(&self) -> &Self::Inner {
-//         panic!("called inner on RpcClient")
-//     }
-
-//     fn send_transaction(
-//         &self,
-//         tx: &N::Transaction,
-//     ) -> RpcCall<Self::Transport, N::Transaction, N::Receipt> {
-//         self.prepare("eth_sendTransaction", tx)
-//     }
-// }
+    fn send_transaction(&self, tx: &N::Transaction) -> RpcCall<T, N::Transaction, N::Receipt> {
+        self.prepare("eth_sendTransaction", tx)
+    }
+}
--- crates/transports/src/lib.rs
+++ crates/transports/src/lib.rs
@@ -19,18 +19,3 @@ mod transports;
 pub use transports::{Http, Transport};
 
 pub use alloy_json_rpc::RpcResult;
-
-#[cfg(test)]
-mod test {
-    use tower::util::BoxCloneService;
-
-    use super::*;
-
-    fn box_clone_transport() -> BoxCloneService<
-        Box<serde_json::value::RawValue>,
-        Box<serde_json::value::RawValue>,
-        TransportError,
-    > {
-        BoxCloneService::new(Http::new("http://localhost:8545".parse().unwrap()))
-    }
-}
