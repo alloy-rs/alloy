6012b0a7b3ca51abbc8f3e59b6a7b953ebb25041
Enrique Ortiz <hi@enriqueortiz.dev>
2023-11-01 15:08:01 -0400

    chore: doc fixes

--- crates/transports/src/transports/json.rs
+++ crates/transports/src/transports/json.rs
@@ -6,7 +6,7 @@ use serde_json::value::RawValue;
 use std::{future::Future, pin::Pin, task};
 use tower::Service;
 
-/// A service layer that transforms [`RequestPacket`] into [`ResponsePacket`]
+/// A service layer that transforms [`RequestPacket`] into [`Response`]
 /// by wrapping an inner service that implements [`Transport`].
 #[derive(Debug, Clone)]
 #[repr(transparent)]
