5d3f5a11a93582853387af1be33feb59ddea31fb
Enrique Ortiz <hi@enriqueortiz.dev>
2023-12-08 11:29:11 -0400

    chore: re-add normal test

--- crates/rpc-client/tests/it/ipc.rs
+++ crates/rpc-client/tests/it/ipc.rs
@@ -24,32 +24,9 @@ async fn connect() -> (RpcClient<PubSubFrontend>, GethInstance) {
     (client, geth)
 }
 
-/// Connects to a local IPC socket. Prints the ipc path to stdout
-/// so a geth instance can be manually booted with the command:
-/// geth --dev --verbosity 4 --http --ipcpath IPC_PATH
-/// The client will auto-connect to the geth instance after 30 seconds.
-async fn connect_2() -> RpcClient<PubSubFrontend> {
-    let temp_file = NamedTempFile::new().unwrap();
-    let path = temp_file.into_temp_path().to_path_buf();
-    tokio::time::sleep(std::time::Duration::from_secs(30)).await;
-    // let geth = Geth::new().block_time(1u64).ipc_path(&path).spawn();
-
-    // [Windows named pipes](https://learn.microsoft.com/en-us/windows/win32/ipc/named-pipes)
-    // are located at `\\<machine_address>\pipe\<pipe_name>`.
-    #[cfg(windows)]
-    let path = format!(r"\\.\pipe\{}", path.display());
-
-    println!("{:?}", path);
-    let connector: IpcConnect<_> = path.into();
-
-    let client = ClientBuilder::default().pubsub(connector).await.unwrap();
-
-    client
-}
-
 #[test_log::test(tokio::test)]
 async fn it_makes_a_request() {
-    let client = connect_2().await;
+    let (client, _geth) = connect().await;
 
     let params: Cow<'static, _> = Cow::Owned(vec![]);
 
