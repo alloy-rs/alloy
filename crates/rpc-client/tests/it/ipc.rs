10ce88a33a3faa9aebe72c23f3a370fe28f9b97d
Enrique Ortiz <hi@enriqueortiz.dev>
2023-12-08 16:13:23 -0400

    chore: revert to geth impl for now

--- crates/rpc-client/tests/it/ipc.rs
+++ crates/rpc-client/tests/it/ipc.rs
@@ -1,22 +1,32 @@
 use std::borrow::Cow;
 
 use alloy_primitives::U64;
-use alloy_rpc_client::{ClientBuilder, RpcCall};
-use alloy_transport_ipc::{IpcConnect, MockIpcServer};
+use alloy_pubsub::PubSubFrontend;
+use alloy_rpc_client::{ClientBuilder, RpcCall, RpcClient};
+use alloy_transport_ipc::IpcConnect;
+use ethers_core::utils::{Geth, GethInstance};
+use tempfile::NamedTempFile;
 
-#[test_log::test(tokio::test)]
-async fn it_makes_a_request() {
-    let mut server = MockIpcServer::new();
+async fn connect() -> (RpcClient<PubSubFrontend>, GethInstance) {
+    let temp_file = NamedTempFile::new().unwrap();
+    let path = temp_file.into_temp_path().to_path_buf();
+    let geth = Geth::new().block_time(1u64).ipc_path(&path).spawn();
 
-    server.add_reply("{\"jsonrpc\": \"2.0\",\"id\": 1,\"result\": \"0x0\"}");
-
-    let path = server.path();
-
-    let _ = server.spawn().await;
+    // [Windows named pipes](https://learn.microsoft.com/en-us/windows/win32/ipc/named-pipes)
+    // are located at `\\<machine_address>\pipe\<pipe_name>`.
+    #[cfg(windows)]
+    let path = format!(r"\\.\pipe\{}", path.display());
 
     let connector: IpcConnect<_> = path.into();
 
     let client = ClientBuilder::default().pubsub(connector).await.unwrap();
+
+    (client, geth)
+}
+
+#[test_log::test(tokio::test)]
+async fn it_makes_a_request() {
+    let (client, _geth) = connect().await;
 
     let params: Cow<'static, _> = Cow::Owned(vec![]);
 
