826365252f183d9740dfada2462d6146c1d1b014
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-01-13 22:28:34 +0100

    chore: cleanup rpc types

--- crates/rpc-trace-types/src/lib.rs
+++ crates/rpc-trace-types/src/lib.rs
@@ -1,4 +1,19 @@
-//! Types for tracing
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
+    html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
+)]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    clippy::missing_const_for_fn,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 pub mod common;
 pub mod filter;
