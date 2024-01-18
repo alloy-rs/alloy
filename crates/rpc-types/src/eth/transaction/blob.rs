217687d95e2c4c7a4b072724e4d5450b1f272cdd
Matthias Seitz <matthias.seitz@outlook.de>
2024-01-18 20:10:08 +0100

    chore: move blob tx sidecar

---
+++ crates/rpc-types/src/eth/transaction/blob.rs
@@ -0,0 +1,16 @@
+//! EIP-4844 related types.
+
+use crate::kzg::{Blob, Bytes48};
+use serde::{Deserialize, Serialize};
+
+/// This represents a set of blobs, and its corresponding commitments and proofs.
+#[derive(Clone, Debug, PartialEq, Eq, Default, Serialize, Deserialize)]
+#[repr(C)]
+pub struct BlobTransactionSidecar {
+    /// The blob data.
+    pub blobs: Vec<Blob>,
+    /// The blob commitments.
+    pub commitments: Vec<Bytes48>,
+    /// The blob proofs.
+    pub proofs: Vec<Bytes48>,
+}
