b81c1f2471c8450c1e0864c7e959e1b053897bd7
AntwuanDixon <evan@struct.fi>
2024-02-26 17:22:18 -0800

    refactor(test): remove struct FooBarNoFizz in favor of FooBar; add serde::Serialize trait to FooBar

--- crates/signer/src/wallet/private_key.rs
+++ crates/signer/src/wallet/private_key.rs
@@ -343,22 +343,11 @@ mod tests {
         use serde::Serialize;
 
         sol! {
-            #[derive(Debug)]
+            #[derive(Debug, Serialize)]
             struct FooBar {
                 int256 foo;
                 uint256 bar;
                 bytes fizz;
-                bytes32 buzz;
-                string far;
-                address out;
-            }
-        }
-
-        sol! {
-            #[derive(Debug, Serialize)]
-            struct FooBarNoFizz {
-                int256 foo;
-                uint256 bar;
                 bytes32 buzz;
                 string far;
                 address out;
@@ -385,16 +374,8 @@ mod tests {
         let sig = wallet.sign_typed_data_sync(&foo_bar, &domain).unwrap();
         assert_eq!(sig.recover_address_from_prehash(&hash).unwrap(), wallet.address());
         assert_eq!(wallet.sign_hash_sync(hash).unwrap(), sig);
-
-        let foo_bar_no_fizz = FooBarNoFizz {
-            foo: I256::try_from(10u64).unwrap(),
-            bar: U256::from(20u64),
-            buzz: keccak256("buzz"),
-            far: String::from("space"),
-            out: Address::ZERO,
-        };
         
-        let foo_bar_dynamic = TypedData::from_struct(&foo_bar_no_fizz, Some(domain));
+        let foo_bar_dynamic = TypedData::from_struct(&foo_bar, Some(domain));
         let dynamic_hash = foo_bar_dynamic.eip712_signing_hash().unwrap();
 
         let sig_dynamic = wallet.sign_dynamic_typed_data_sync(&foo_bar_dynamic).unwrap();
