6e1b1ee34a9fd6139c3f6f227c7a9fc70b061bb7
Oliver Nordbjerg <hi@notbjerg.me>
2024-03-07 16:19:21 +0100

    feat: network signer trait

---
+++ crates/network/src/transaction/signer.rs
@@ -0,0 +1,32 @@
+use crate::Network;
+use alloy_consensus::SignableTransaction;
+use async_trait::async_trait;
+
+// todo: move
+/// A signer capable of signing any transaction for the given network.
+#[async_trait]
+pub trait NetworkSigner<N: Network> {
+    /// Asynchronously sign an unsigned transaction.
+    async fn sign(&self, tx: N::UnsignedTx) -> alloy_signer::Result<N::TxEnvelope>;
+}
+
+// todo: move
+/// An async signer capable of signing any [SignableTransaction] for the given [Signature] type.
+#[async_trait]
+pub trait TxSigner<Signature> {
+    /// Asynchronously sign an unsigned transaction.
+    async fn sign_transaction(
+        &self,
+        tx: &mut dyn SignableTransaction<Signature>,
+    ) -> alloy_signer::Result<Signature>;
+}
+
+// todo: move
+/// A sync signer capable of signing any [SignableTransaction] for the given [Signature] type.
+pub trait TxSignerSync<Signature> {
+    /// Synchronously sign an unsigned transaction.
+    fn sign_transaction_sync(
+        &self,
+        tx: &mut dyn SignableTransaction<Signature>,
+    ) -> alloy_signer::Result<Signature>;
+}
