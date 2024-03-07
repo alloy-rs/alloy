16b328e995a5b0ef6261bad9f34aebb69787ee6f
Oliver Nordbjerg <hi@notbjerg.me>
2024-03-07 17:07:32 +0100

    temp: remove `Hash` derive

--- crates/consensus/src/transaction/typed.rs
+++ crates/consensus/src/transaction/typed.rs
@@ -8,7 +8,7 @@ use alloy_primitives::TxKind;
 /// 2. EIP2930 (state access lists) [`TxEip2930`]
 /// 3. EIP1559 [`TxEip1559`]
 /// 4. EIP4844 [`TxEip4844`]
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TypedTransaction {
     /// Legacy transaction
     Legacy(TxLegacy),
