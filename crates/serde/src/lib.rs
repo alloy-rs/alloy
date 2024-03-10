4983b27c5bd2429eb6a36a65a6b7d1e30b815bd0
moricho <ikeda.morito@gmail.com>
2024-03-10 20:22:42 +0900

    Add header to lib.rs

--- crates/serde/src/lib.rs
+++ crates/serde/src/lib.rs
@@ -1,5 +1,22 @@
 //! Alloy serde helpers for primitive types.
 
+#![doc = include_str!("../README.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
+    html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
+)]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    unreachable_pub,
+    clippy::missing_const_for_fn,
+    rustdoc::all
+)]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![deny(unused_must_use, rust_2018_idioms)]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+
 use alloy_primitives::B256;
 use serde::Serializer;
 
