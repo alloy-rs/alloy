b20f78575974e5d3bc7a3d6f4a82e4b82727e5af
zerosnacks <zerosnacks@protonmail.com>
2024-03-12 15:03:23 +0000

    add note on rand

--- examples/wallets/examples/mnemonic.rs
+++ examples/wallets/examples/mnemonic.rs
@@ -2,6 +2,9 @@
 
 use alloy_signer::{coins_bip39::English, MnemonicBuilder};
 
+// NOTE: `rand` is not exported anymore, now private.
+use rand;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let phrase = "work man father plunge mystery proud hollow address reunion sauce theory bonus";
@@ -17,7 +20,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .password(password)
         .build()?;
 
-    eprintln!("Wallet: {wallet:?}");
+    println!("Wallet: {:?}", wallet);
+
+    // Generate a random wallet (24 word phrase) at custom derivation path
+    let mut rng = rand::thread_rng();
+    let wallet = MnemonicBuilder::<English>::default()
+        .word_count(24)
+        .derivation_path("m/44'/60'/0'/2/1")?
+        // Optionally add this if you want the generated mnemonic to be written
+        // to a file
+        // .write_to(path)
+        .build_random(&mut rng)?;
+
+    println!("Random wallet: {:?}", wallet);
 
     Ok(())
 }
