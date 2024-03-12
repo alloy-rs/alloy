9aa0d51031fd1b0dff5ead727edb86b7f9aa1668
zerosnacks <zerosnacks@protonmail.com>
2024-03-12 12:31:37 +0000

    add basic http provider

--- examples/wallets/examples/ledger.rs
+++ examples/wallets/examples/ledger.rs
@@ -1,6 +1,19 @@
 //! # Ledger Wallet Example
 
+use alloy_network::Ethereum;
+use alloy_providers::{HttpProvider, Provider};
+use alloy_rpc_client::RpcClient;
+use alloy_transport_http::Http;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let transport = Http::new("http://localhost:8545".parse()?);
+    let client = RpcClient::new(transport, true);
+    let provider = HttpProvider::<Ethereum>::new(client);
+
+    let block_number = provider.get_block_number().await.unwrap();
+
+    println!("Block number: {}", block_number);
+
     Ok(())
 }
