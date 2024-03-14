d75d0ee0bd37ca4108810b44b1b0111416e42fa3
zerosnacks <zerosnacks@protonmail.com>
2024-03-14 15:20:12 +0000

    init

---
+++ examples/layers/examples/nonce_layer.rs
@@ -0,0 +1,52 @@
+//! Example of the signer layer.
+
+use alloy_network::{Ethereum, EthereumSigner};
+use alloy_primitives::{address, U256};
+use alloy_provider::{
+    layers::{self, ManagedNonceLayer},
+    Provider, ProviderBuilder, RootProvider,
+};
+use alloy_rpc_client::RpcClient;
+use alloy_rpc_types::request::TransactionRequest;
+use alloy_signer_ledger::{HDPath, LedgerSigner};
+use alloy_transport_http::Http;
+use reqwest::Client;
+
+/// In Ethereum, the nonce of a transaction is a number that represents the number of transactions
+/// that have been sent from a particular account. The nonce is used to ensure that transactions are
+/// processed in the order they are intended, and to prevent the same transaction from being
+/// processed multiple times.
+///
+/// The nonce manager in Alloy is a layer that helps you manage the nonce
+/// of transactions by keeping track of the current nonce for a given account and automatically
+/// incrementing it as needed. This can be useful if you want to ensure that transactions are sent
+/// in the correct order, or if you want to avoid having to manually manage the nonce yourself.
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Instantiate the application by acquiring a lock on the Ledger device.
+    let signer = LedgerSigner::new(HDPath::LedgerLive(0), Some(1)).await?;
+
+    // Create a provider with the signer and the network.
+    let http = Http::<Client>::new("http://localhost:8545".parse()?);
+    let provider = ProviderBuilder::<_, Ethereum>::new()
+        .signer(EthereumSigner::from(signer))
+        .layer(layers::ManagedNonceLayer)
+        .network::<Ethereum>()
+        .provider(RootProvider::new(RpcClient::new(http, true)));
+
+    // Create a transaction.
+    let tx = TransactionRequest {
+        value: Some(U256::from(100)),
+        to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+        gas_price: Some(U256::from(20e9)),
+        gas: Some(U256::from(21000)),
+        ..Default::default()
+    };
+
+    // Broadcast the transaction and wait for the receipt.
+    let receipt = provider.send_transaction(tx).await?.with_confirmations(3).get_receipt().await?;
+
+    println!("Send transaction: {:?}", receipt.transaction_hash.unwrap());
+
+    Ok(())
+}
