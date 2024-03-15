89bc757e5aafc4fec3f2a542f9974f95a717f942
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-03-15 15:54:11 -0400

    feat(layers): GasEstimatorLayer

--- crates/provider/src/layers/gas.rs
+++ crates/provider/src/layers/gas.rs
@@ -1,5 +1,5 @@
 use crate::{PendingTransactionBuilder, Provider, ProviderLayer, RootProvider};
-use alloy_network::Network;
+use alloy_network::{Network, TransactionBuilder};
 use alloy_primitives::U256;
 use alloy_transport::{Transport, TransportResult};
 use async_trait::async_trait;
@@ -17,11 +17,12 @@ where
     T: Transport + Clone,
 {
     type Provider = GasEstimatorProvider<N, T, P>;
-    fn layer(&self, _inner: P) -> Self::Provider {
-        todo!()
+    fn layer(&self, inner: P) -> Self::Provider {
+        GasEstimatorProvider { inner, _phantom: PhantomData }
     }
 }
 
+/// A provider that provides gas estimation for transactions.
 #[derive(Debug, Clone)]
 pub struct GasEstimatorProvider<N, T, P>
 where
@@ -40,18 +41,13 @@ where
     P: Provider<N, T>,
 {
     async fn get_gas_price(&self) -> TransportResult<U256> {
-        todo!()
+        let gas_price = self.inner.get_gas_price().await?;
+        Ok(gas_price)
     }
 
-    async fn get_gas_estimate(&self, _tx: N::TransactionRequest) -> TransportResult<U256> {
-        todo!()
-    }
-
-    async fn get_1559_estimate_fees(
-        &self,
-        _tx: N::TransactionRequest,
-    ) -> TransportResult<(U256, U256)> {
-        todo!()
+    async fn get_gas_estimate(&self, tx: &N::TransactionRequest) -> TransportResult<U256> {
+        let gas_estimate = self.inner.estimate_gas(tx, None).await?;
+        Ok(gas_estimate)
     }
 }
 
@@ -71,7 +67,60 @@ where
         &self,
         mut tx: N::TransactionRequest,
     ) -> TransportResult<PendingTransactionBuilder<'_, N, T>> {
-        // TODO: Get estimates and set the gas for the tx.
+        if tx.gas_price().is_none() {
+            let gas_price = self.get_gas_price().await?;
+            tx.set_gas_price(gas_price);
+        }
+        if tx.gas_limit().is_none() {
+            let gas_estimate = self.get_gas_estimate(&tx).await?;
+            tx.set_gas_limit(gas_estimate);
+        }
+
         self.inner.send_transaction(tx).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{layers::ManagedNonceLayer, ProviderBuilder};
+    use alloy_network::EthereumSigner;
+    use alloy_node_bindings::Anvil;
+    use alloy_primitives::{address, U128, U256};
+    use alloy_rpc_client::RpcClient;
+    use alloy_rpc_types::TransactionRequest;
+    use alloy_transport_http::Http;
+    use reqwest::Client;
+
+    #[tokio::test]
+    async fn no_gas_price_or_limit() {
+        let anvil = Anvil::new().spawn();
+        let url = anvil.endpoint().parse().unwrap();
+        let http = Http::<Client>::new(url);
+
+        let wallet = alloy_signer_wallet::Wallet::from(anvil.keys()[0].clone());
+
+        let provider = ProviderBuilder::new()
+            .layer(ManagedNonceLayer)
+            .layer(GasEstimatorLayer)
+            .signer(EthereumSigner::from(wallet))
+            .provider(RootProvider::new(RpcClient::new(http, true)));
+
+        let tx = TransactionRequest {
+            from: Some(anvil.addresses()[0]),
+            value: Some(U256::from(100)),
+            to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            ..Default::default()
+        };
+
+        let tx = provider.send_transaction(tx).await.unwrap();
+
+        let tx = tx.get_receipt().await.unwrap();
+
+        let set_gas_price = U128::from(0x77359400);
+        let set_gas_limit = U256::from(0x5208);
+
+        assert_eq!(tx.effective_gas_price, set_gas_price);
+        assert_eq!(tx.gas_used, Some(set_gas_limit));
     }
 }
