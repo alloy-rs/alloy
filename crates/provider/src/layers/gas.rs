e5c8bf1bc07ff9db735eac8f86974eab1e1c3628
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-03-20 15:12:34 -0400

    handle 1559 and legacy txs

--- crates/provider/src/layers/gas.rs
+++ crates/provider/src/layers/gas.rs
@@ -1,12 +1,42 @@
 use crate::{PendingTransactionBuilder, Provider, ProviderLayer, RootProvider};
 use alloy_network::{Network, TransactionBuilder};
 use alloy_primitives::U256;
-use alloy_transport::{Transport, TransportResult};
+use alloy_transport::{Transport, TransportError, TransportResult};
 use async_trait::async_trait;
 use std::marker::PhantomData;
 
-/// A layer that provides gas estimation for transactions.
-/// Populates the gas price and gas limit for transactions.
+/// A layer that populates gas related fields in transaction requests if unset.
+///
+/// Gas related fields are gas_price, gas_limit, max_fee_per_gas and max_priority_fee_per_gas.
+///
+/// The layer fetches the estimations for these via the [`Provider::get_gas_price`],
+/// [`Provider::estimate_gas`] and [`Provider::estimate_eip1559_fees`] methods.
+///
+/// If you use layers that redirect the behavior of [`Provider::send_transaction`]
+/// (e.g. [`SignerLayer`]), you should add this layer before those.
+///
+/// Note:
+///     - If none of the gas related fields are set, the layer first assumes it's a EIP-1559 tx and
+///       populates the gas_limit, max_fee_per_gas and max_priority_fee_per_gas fields.
+///     - If the network does not support EIP-1559, it will process as a legacy tx and populate the
+///       gas_limit and gas_price fields.
+///     - If the gas_price is already set by the user, it will process as a legacy tx and populate
+///       the gas_limit field if unset.
+///
+/// # Example
+///
+/// ```rs
+/// # async fn test<T: Transport + Clone, S: NetworkSigner<Ethereum>>(transport: T, signer: S) {
+/// let provider = ProviderBuilder::new()
+///     .layer(ManagedNonceLayer)
+///     .layer(GasEstimatorLayer)
+///     .signer(EthereumSigner::from(signer)) // note the order!
+///     .provider(RootProvider::new(transport));
+///
+/// provider.send_transaction(TransactionRequest::default()).await;
+/// # }
+///
+/// [`SignerLayer`]: crate::layers::SignerLayer
 #[derive(Debug, Clone, Copy)]
 pub struct GasEstimatorLayer;
 
@@ -22,7 +52,14 @@ where
     }
 }
 
-/// A provider that provides gas estimation for transactions.
+/// A provider that estimates gas for transactions.
+///
+/// Note: This provider requires the chain_id to be set in the transaction request if it's a
+/// EIP1559.
+///
+/// You cannot construct this directly, use [`ProviderBuilder`] with a [`GasEstimatorLayer`].
+///
+/// [`ProviderBuilder`]: crate::ProviderBuilder
 #[derive(Debug, Clone)]
 pub struct GasEstimatorProvider<N, T, P>
 where
@@ -40,21 +77,154 @@ where
     T: Transport + Clone,
     P: Provider<N, T>,
 {
+    /// Gets the gas_price to be used in legacy txs.
     async fn get_gas_price(&self) -> TransportResult<U256> {
-        let gas_price = self.inner.get_gas_price().await?;
-        Ok(gas_price)
+        self.inner.get_gas_price().await
     }
 
+    /// Gets the gas_limit to be used in txs.
     async fn get_gas_estimate(&self, tx: &N::TransactionRequest) -> TransportResult<U256> {
-        let gas_estimate = self.inner.estimate_gas(tx, None).await?;
-        Ok(gas_estimate)
+        self.inner.estimate_gas(tx, None).await
     }
 
-    async fn get_eip1159_fees_estimate(&self) -> TransportResult<(U256, U256)> {
-        let (max_fee_per_gas, max_priority_fee_per_gas) =
-            self.inner.estimate_eip1559_fees(None).await?;
+    /// Gets the max_fee_per_gas and max_priority_fee_per_gas to be used in EIP-1559 txs.
+    async fn get_eip1559_fees_estimate(&self) -> TransportResult<(U256, U256)> {
+        self.inner.estimate_eip1559_fees(None).await
+    }
 
-        Ok((max_fee_per_gas, max_priority_fee_per_gas))
+    /// Populates the gas_limit, max_fee_per_gas and max_priority_fee_per_gas fields if unset.
+    /// Requires the chain_id to be set in the transaction request to be processed as a EIP-1559 tx.
+    /// If the network does not support EIP-1559, it will process it as a legacy tx.
+    async fn handle_eip1559_tx(
+        &self,
+        mut tx: N::TransactionRequest,
+    ) -> Result<N::TransactionRequest, TransportError> {
+        // Check if the user has set the gas_limit, max_fee_per_gas and max_priority_fee_per_gas
+        // fields.
+        let gas_limit_unset = tx.gas_limit().is_none();
+        // eip1559_fees_unset is true if any one of the two max_fee_per_gas or
+        // max_priority_fee_per_gas is unset.
+        let eip1559_fees_unset =
+            tx.max_fee_per_gas().is_none() || tx.max_priority_fee_per_gas().is_none();
+
+        match (gas_limit_unset, eip1559_fees_unset) {
+            (true, true) => {
+                // When both gas_limit and eip1559_fees are unset by the user.
+                let gas_estimate = self.get_gas_estimate(&tx);
+                let eip1559_fees = self.get_eip1559_fees_estimate();
+
+                match futures::join!(gas_estimate, eip1559_fees) {
+                    (Ok(gas_estimate), Ok((max_fee_per_gas, max_priority_fee_per_gas))) => {
+                        tx.set_gas_limit(gas_estimate);
+                        tx.set_max_fee_per_gas(max_fee_per_gas);
+                        tx.set_max_priority_fee_per_gas(max_priority_fee_per_gas);
+
+                        Ok(tx)
+                    }
+                    (Ok(gas_estimate), Err(err)) => {
+                        tx.set_gas_limit(gas_estimate);
+
+                        if err.is_transport_error()
+                            && err.to_string() == *"EIP-1559 not activated".to_string()
+                        {
+                            // If EIP-1559 is not activated, it will process as a legacy tx.
+                            match self.handle_legacy_tx(tx).await {
+                                Ok(tx) => Ok(tx),
+                                Err(err) => Err(err),
+                            }
+                        } else {
+                            Err(err)
+                        }
+                    }
+                    (Err(err), Ok((_max_fee_per_gas, _max_priority_fee_per_gas))) => Err(err),
+                    (Err(err1), Err(_err2)) => Err(err1),
+                }
+            }
+            (true, false) => {
+                // When gas_limit is unset by the user.
+                let gas_estimate = self.get_gas_estimate(&tx).await;
+                match gas_estimate {
+                    Ok(gas_estimate) => {
+                        tx.set_gas_limit(gas_estimate);
+                        Ok(tx)
+                    }
+                    Err(err) => {
+                        if err.is_transport_error()
+                            && err.to_string() == *"EIP-1559 not activated".to_string()
+                        {
+                            // Try legacy tx if the network does not support EIP-1559.
+                            match self.handle_legacy_tx(tx).await {
+                                Ok(tx) => Ok(tx),
+                                Err(err) => Err(err),
+                            }
+                        } else {
+                            Err(err)
+                        }
+                    }
+                }
+            }
+            (false, true) => {
+                // When eip1559_fees are unset by the user.
+                let eip1559_fees = self.get_eip1559_fees_estimate().await;
+                match eip1559_fees {
+                    Ok((max_fee_per_gas, max_priority_fee_per_gas)) => {
+                        tx.set_max_fee_per_gas(max_fee_per_gas);
+                        tx.set_max_priority_fee_per_gas(max_priority_fee_per_gas);
+                        Ok(tx)
+                    }
+                    Err(err) => {
+                        if err.is_transport_error()
+                            && err.to_string() == *"EIP-1559 not activated".to_string()
+                        {
+                            // Try legacy tx if the network does not support EIP-1559.
+                            match self.handle_legacy_tx(tx).await {
+                                Ok(tx) => Ok(tx),
+                                Err(err) => Err(err),
+                            }
+                        } else {
+                            Err(err)
+                        }
+                    }
+                }
+            }
+            _ => {
+                // When both gas_limit and eip1559_fees are set by the user.
+                Ok(tx)
+            }
+        }
+    }
+
+    /// Populates the gas_price and only populates the gas_limit field if unset.
+    /// This method always assumes that the gas_price is unset.
+    async fn handle_legacy_tx(
+        &self,
+        mut tx: N::TransactionRequest,
+    ) -> Result<N::TransactionRequest, TransportError> {
+        let gas_price = self.get_gas_price();
+
+        if tx.gas_limit().is_none() {
+            let gas_estimate = self.get_gas_estimate(&tx);
+
+            match futures::join!(gas_price, gas_estimate) {
+                (Ok(gas_price), Ok(gas_estimate)) => {
+                    tx.set_gas_price(gas_price);
+                    tx.set_gas_limit(gas_estimate);
+                    Ok(tx)
+                }
+                (Ok(_gas_price), Err(err)) => Err(err),
+                (Err(err), Ok(_gas_estimate)) => Err(err),
+                (Err(err1), Err(_err2)) => Err(err1),
+            }
+        } else {
+            let gas_price = gas_price.await;
+            match gas_price {
+                Ok(gas_price) => {
+                    tx.set_gas_price(gas_price);
+                    Ok(tx)
+                }
+                Err(err) => Err(err),
+            }
+        }
     }
 }
 
@@ -74,88 +244,15 @@ where
         &self,
         mut tx: N::TransactionRequest,
     ) -> TransportResult<PendingTransactionBuilder<'_, N, T>> {
-        let (gas_price, gas_limit, eip1559_fees) = match (
-            tx.gas_price().is_none(),
-            tx.gas_limit().is_none(),
-            (tx.max_fee_per_gas().is_none() && tx.max_priority_fee_per_gas().is_none()),
-        ) {
-            (true, true, true) => {
-                let gas_price = self.get_gas_price();
-                let gas_estimate = self.get_gas_estimate(&tx);
-                let eip1559_fees = self.get_eip1159_fees_estimate();
-
-                match futures::join!(gas_price, gas_estimate, eip1559_fees) {
-                    (
-                        Ok(gas_price),
-                        Ok(gas_estimate),
-                        Ok((max_fee_per_gas, max_priority_fee_per_gas)),
-                    ) => (
-                        Some(gas_price),
-                        Some(gas_estimate),
-                        Some((max_fee_per_gas, max_priority_fee_per_gas)),
-                    ),
-                    _ => (None, None, None),
-                }
-            }
-            (true, true, false) => {
-                let gas_price = self.get_gas_price();
-                let gas_estimate = self.get_gas_estimate(&tx);
-
-                match futures::join!(gas_price, gas_estimate) {
-                    (Ok(gas_price), Ok(gas_estimate)) => {
-                        (Some(gas_price), Some(gas_estimate), None)
-                    }
-                    _ => (None, None, None),
-                }
-            }
-            (true, false, true) => {
-                let gas_price = self.get_gas_price();
-                let eip1559_fees = self.get_eip1159_fees_estimate();
-
-                match futures::join!(gas_price, eip1559_fees) {
-                    (Ok(gas_price), Ok((max_fee_per_gas, max_priority_fee_per_gas))) => {
-                        (Some(gas_price), None, Some((max_fee_per_gas, max_priority_fee_per_gas)))
-                    }
-                    _ => (None, None, None),
-                }
-            }
-            (true, false, false) => {
-                let gas_price = self.get_gas_price().await?;
-                (Some(gas_price), None, None)
-            }
-            (false, true, true) => {
-                let gas_estimate = self.get_gas_estimate(&tx);
-                let eip1559_fees = self.get_eip1159_fees_estimate();
-
-                match futures::join!(gas_estimate, eip1559_fees) {
-                    (Ok(gas_estimate), Ok((max_fee_per_gas, max_priority_fee_per_gas))) => (
-                        None,
-                        Some(gas_estimate),
-                        Some((max_fee_per_gas, max_priority_fee_per_gas)),
-                    ),
-                    _ => (None, None, None),
-                }
-            }
-            (false, false, true) => {
-                let (max_fee_per_gas, max_priority_fee_per_gas) =
-                    self.get_eip1159_fees_estimate().await?;
-
-                (None, None, Some((max_fee_per_gas, max_priority_fee_per_gas)))
-            }
-            _ => (None, None, None),
-        };
-
-        if let Some(gas_price) = gas_price {
-            tx.set_gas_price(gas_price);
-        }
-
-        if let Some(gas_limit) = gas_limit {
-            tx.set_gas_limit(gas_limit);
-        }
-
-        if let Some((max_fee_per_gas, max_priority_fee_per_gas)) = eip1559_fees {
-            tx.set_max_fee_per_gas(max_fee_per_gas);
-            tx.set_max_priority_fee_per_gas(max_priority_fee_per_gas);
+        if tx.gas_price().is_none() {
+            // Assume its a EIP1559 tx
+            // Populate the following gas_limit, max_fee_per_gas and max_priority_fee_per_gas fields
+            // if unset.
+            tx = self.handle_eip1559_tx(tx.clone()).await?;
+        } else {
+            // Assume its a legacy tx
+            // Populate only the gas_limit field if unset.
+            tx = self.handle_legacy_tx(tx.clone()).await?;
         }
         self.inner.send_transaction(tx).await
     }
@@ -187,10 +284,12 @@ mod tests {
             .signer(EthereumSigner::from(wallet))
             .provider(RootProvider::new(RpcClient::new(http, true)));
 
+        // GasEstimationLayer requires chain_id to be set to handle EIP-1559 tx
         let tx = TransactionRequest {
             from: Some(anvil.addresses()[0]),
             value: Some(U256::from(100)),
             to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            chain_id: Some(31337),
             ..Default::default()
         };
 
@@ -198,10 +297,73 @@ mod tests {
 
         let tx = tx.get_receipt().await.unwrap();
 
-        let set_gas_price = U128::from(0x77359400);
+        let set_gas_price = U128::from(0x3b9aca00);
         let set_gas_limit = U256::from(0x5208);
 
         assert_eq!(tx.effective_gas_price, set_gas_price);
         assert_eq!(tx.gas_used, Some(set_gas_limit));
+    }
+
+    #[tokio::test]
+    async fn no_gas_limit() {
+        let anvil = Anvil::new().spawn();
+        let url = anvil.endpoint().parse().unwrap();
+        let http = Http::<Client>::new(url);
+
+        let wallet = alloy_signer_wallet::Wallet::from(anvil.keys()[0].clone());
+
+        let provider = ProviderBuilder::new()
+            .layer(ManagedNonceLayer)
+            .layer(GasEstimatorLayer)
+            .signer(EthereumSigner::from(wallet))
+            .provider(RootProvider::new(RpcClient::new(http, true)));
+
+        let gas_price = provider.get_gas_price().await.unwrap();
+        let tx = TransactionRequest {
+            from: Some(anvil.addresses()[0]),
+            value: Some(U256::from(100)),
+            to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            gas_price: Some(gas_price),
+            ..Default::default()
+        };
+
+        let tx = provider.send_transaction(tx).await.unwrap();
+
+        let tx = tx.get_receipt().await.unwrap();
+
+        let set_gas_limit = U256::from(0x5208);
+
+        assert_eq!(tx.gas_used, Some(set_gas_limit));
+    }
+
+    #[tokio::test]
+    async fn non_eip1559_network() {
+        let anvil = Anvil::new().arg("--hardfork").arg("frontier").spawn();
+        let url = anvil.endpoint().parse().unwrap();
+        let http = Http::<Client>::new(url);
+
+        let wallet = alloy_signer_wallet::Wallet::from(anvil.keys()[0].clone());
+
+        let provider = ProviderBuilder::new()
+            .layer(ManagedNonceLayer)
+            .layer(GasEstimatorLayer)
+            .signer(EthereumSigner::from(wallet))
+            .provider(RootProvider::new(RpcClient::new(http, true)));
+
+        let tx = TransactionRequest {
+            from: Some(anvil.addresses()[0]),
+            value: Some(U256::from(100)),
+            to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            // chain_id: Some(31337), Not required as this will fallback to legacy_tx
+            ..Default::default()
+        };
+
+        let tx = provider.send_transaction(tx).await.unwrap();
+
+        let tx = tx.get_receipt().await.unwrap();
+
+        let set_gas_price = U128::from(0x6fc23ac0);
+
+        assert_eq!(tx.effective_gas_price, set_gas_price);
     }
 }
