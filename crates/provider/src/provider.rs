4c6fe3ce52efd4f3df34d22a74aedf56e6921be8
Developer Uche <69772615+developeruche@users.noreply.github.com>
2024-03-21 05:58:06 +0100

    modification: Eip1559Estimation return type (#352)
    * modification Eip1559Estimation return type
    
    * chore: fmt
    
    ---------
    
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/provider/src/provider.rs
+++ crates/provider/src/provider.rs
@@ -3,7 +3,7 @@
 use crate::{
     chain::ChainStreamPoller,
     heart::{Heartbeat, HeartbeatHandle, PendingTransaction, PendingTransactionConfig},
-    utils::{self, EstimatorFunction},
+    utils::{self, Eip1559Estimation, EstimatorFunction},
     PendingTransactionBuilder,
 };
 use alloy_json_rpc::{RpcParam, RpcReturn};
@@ -546,9 +546,9 @@ pub trait Provider<N: Network, T: Transport + Clone = BoxTransport>: Send + Sync
     ) -> TransportResult<()> {
         let gas = self.estimate_eip1559_fees(estimator).await;
 
-        gas.map(|(max_fee_per_gas, max_priority_fee_per_gas)| {
-            tx.set_max_fee_per_gas(max_fee_per_gas);
-            tx.set_max_priority_fee_per_gas(max_priority_fee_per_gas);
+        gas.map(|estimate| {
+            tx.set_max_fee_per_gas(estimate.max_fee_per_gas);
+            tx.set_max_priority_fee_per_gas(estimate.max_priority_fee_per_gas);
         })
     }
 
@@ -761,7 +761,7 @@ pub trait Provider<N: Network, T: Transport + Clone = BoxTransport>: Send + Sync
     async fn estimate_eip1559_fees(
         &self,
         estimator: Option<EstimatorFunction>,
-    ) -> TransportResult<(U256, U256)> {
+    ) -> TransportResult<Eip1559Estimation> {
         let base_fee_per_gas = match self.get_block_by_number(BlockNumberOrTag::Latest, false).await
         {
             Ok(Some(block)) => match block.header.base_fee_per_gas {
@@ -796,7 +796,7 @@ pub trait Provider<N: Network, T: Transport + Clone = BoxTransport>: Send + Sync
             )
         };
 
-        Ok((max_fee_per_gas, max_priority_fee_per_gas))
+        Ok(Eip1559Estimation { max_fee_per_gas, max_priority_fee_per_gas })
     }
 
     /// Get the account and storage values of the specified account including the merkle proofs.
