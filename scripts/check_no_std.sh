a90332fd5e930043bb44f50da142c22e7f4a1bfd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-03-22 15:55:20 +0100

    ci: check no_std in CI (#367)
    * chore: touch up default features for no_std
    
    * ci: check no_std in ci
    
    * docs: update README.md
    
    * fix
    
    * docs

---
+++ scripts/check_no_std.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+no_std_packages=(
+    alloy-eips
+    alloy-genesis
+    alloy-serde
+)
+
+for package in "${no_std_packages[@]}"; do
+  cmd="cargo +stable build -p $package --target riscv32imac-unknown-none-elf --no-default-features"
+  if [ -n "$CI" ]; then
+    echo "::group::$cmd"
+  else
+    printf "\n%s:\n  %s\n" "$package" "$cmd"
+  fi
+
+  $cmd
+
+  if [ -n "$CI" ]; then
+    echo "::endgroup::"
+  fi
+done
