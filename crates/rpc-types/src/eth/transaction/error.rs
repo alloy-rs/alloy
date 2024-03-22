117910cff631bb6412561729c52dabe75ba7d2cd
James Prestwich <james@prestwi.ch>
2024-03-22 08:35:52 -0700

    fix: missing to in 4844 conversion (#366)
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/rpc-types/src/eth/transaction/error.rs
+++ crates/rpc-types/src/eth/transaction/error.rs
@@ -4,24 +4,34 @@
 #[allow(missing_copy_implementations)]
 #[allow(missing_docs)]
 pub enum ConversionError {
+    /// Missing `gasPrice` field for Legacy transaction.
     #[error("missing `gasPrice` field for Legacy transaction")]
     MissingGasPrice,
+    /// Missing signature for transaction.
     #[error("missing signature for transaction")]
     MissingSignature,
+    /// Missing `accessList` field for EIP-2930 transaction.
     #[error("missing `accessList` field for EIP-2930 transaction")]
     MissingAccessList,
+    /// Missing `maxFeePerGas` field for EIP-1559 transaction.
     #[error("missing `maxFeePerGas` field for EIP-1559 transaction")]
     MissingMaxFeePerGas,
-    #[error("missing `maxPriorityFeePerGas` field for EIP-1559 transaction")]
-    MissingMaxPriorityFeePerGas,
-    #[error("missing `maxFeePerBlobGas` field for EIP-1559 transaction")]
-    MissingMaxFeePerBlobGas,
-    #[error("missing `chainId` field for EIP-155 transaction")]
-    MissingChainId,
+    /// Missing `to` field for EIP-4844 transaction.
     #[error("missing `to` field for EIP-4844 transaction")]
     MissingTo,
+    /// Missing `maxPriorityFeePerGas` field for EIP-1559 transaction.
+    #[error("missing `maxPriorityFeePerGas` field for EIP-1559 transaction")]
+    MissingMaxPriorityFeePerGas,
+    /// Missing `maxFeePerBlobGas` field for EIP-1559 transaction.
+    #[error("missing `maxFeePerBlobGas` field for EIP-1559 transaction")]
+    MissingMaxFeePerBlobGas,
+    /// Missing `chainId` field for EIP-1559 transaction.
+    #[error("missing `chainId` field for EIP-155 transaction")]
+    MissingChainId,
+    /// [`alloy_primitives::SignatureError`].
     #[error(transparent)]
     SignatureError(#[from] alloy_primitives::SignatureError),
+    /// Error during EIP-2718 transaction coding.
     #[error(transparent)]
     Eip2718Error(#[from] alloy_eips::eip2718::Eip2718Error),
 }
