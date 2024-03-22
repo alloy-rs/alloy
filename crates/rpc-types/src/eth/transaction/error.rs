5515915a4b0929cc8745eeecb0db6a663de51c1a
Arsenii Kulikov <klkvrr@gmail.com>
2024-03-22 06:38:04 +0400

    feat: `impl TryFrom<Transaction> for TxEnvelope` (#343)
    * feat: impl impl TryFrom<Transaction> for TxEnvelope
    
    * fix
    
    * fix
    
    * wip
    
    * separate error type
    
    * mem
    
    * move to rpc-types
    
    * Update TxType
    
    * fix cargo.toml

---
+++ crates/rpc-types/src/eth/transaction/error.rs
@@ -0,0 +1,24 @@
+#[allow(missing_docs)]
+/// Error variants when converting from [crate::Transaction] to [alloy_consensus::Signed]
+/// transaction.
+#[derive(Debug, thiserror::Error)]
+pub enum ConversionError {
+    #[error("missing `gasPrice` field for Legacy transaction")]
+    MissingGasPrice,
+    #[error("missing signature for transaction")]
+    MissingSignature,
+    #[error("missing `accessList` field for EIP-2930 transaction")]
+    MissingAccessList,
+    #[error("missing `maxFeePerGas` field for EIP-1559 transaction")]
+    MissingMaxFeePerGas,
+    #[error("missing `maxPriorityFeePerGas` field for EIP-1559 transaction")]
+    MissingMaxPriorityFeePerGas,
+    #[error("missing `maxFeePerBlobGas` field for EIP-1559 transaction")]
+    MissingMaxFeePerBlobGas,
+    #[error("missing `chainId` field for EIP-155 transaction")]
+    MissingChainId,
+    #[error(transparent)]
+    SignatureError(#[from] alloy_primitives::SignatureError),
+    #[error(transparent)]
+    Eip2718Error(#[from] alloy_eips::eip2718::Eip2718Error),
+}
