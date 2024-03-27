926e4a3e64a065a9f15c0848aca1918f480686d5
James <james@prestwi.ch>
2024-03-27 09:57:14 -0700

    lint: clippy

--- crates/rpc-types/src/eth/transaction/receipt.rs
+++ crates/rpc-types/src/eth/transaction/receipt.rs
@@ -72,7 +72,7 @@ impl TransactionReceipt {
     }
 
     /// Returns the transaction type.
-    pub fn transaction_type(&self) -> TxType {
+    pub const fn transaction_type(&self) -> TxType {
         self.inner.tx_type()
     }
 
