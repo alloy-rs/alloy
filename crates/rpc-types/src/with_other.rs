d3a7ff067620d16ab62d448fc8f11bed85826043
Aurélien <3535019+leruaa@users.noreply.github.com>
2024-03-30 00:12:17 +0100

    feat: add `AnyNetwork` (#383)
    * feat: add `AnyNetwork`
    
    * fix: apply requested changes
    
    * nit: reorder docs
    
    * nit: move `WithOtherFields` out of eth namespace
    
    * fix: module
    
    * fix: impl `blob_sidecar` methods
    
    * nit: squash imports together
    
    * fix: make `inner` pub

---
+++ crates/rpc-types/src/with_other.rs
@@ -0,0 +1,42 @@
+use crate::other::OtherFields;
+use serde::{Deserialize, Serialize};
+use std::ops::{Deref, DerefMut};
+
+/// Wrapper allowing to catch all fields missing on the inner struct while
+/// deserialize.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WithOtherFields<T> {
+    /// The inner struct.
+    #[serde(flatten)]
+    pub inner: T,
+    /// All fields not present in the inner struct.
+    #[serde(flatten)]
+    pub other: OtherFields,
+}
+
+impl<T> WithOtherFields<T> {
+    /// Create a new `Extra`.
+    pub fn new(inner: T) -> Self {
+        Self { inner, other: OtherFields::default() }
+    }
+}
+
+impl<T> Deref for WithOtherFields<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T> DerefMut for WithOtherFields<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl<T: Default> Default for WithOtherFields<T> {
+    fn default() -> Self {
+        WithOtherFields::new(T::default())
+    }
+}
