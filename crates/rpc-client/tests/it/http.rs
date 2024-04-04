7b0e2cc364063f65a724dda6ee908e86ba571186
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-04-04 19:21:35 +0100

    Removed reqwest prefix

--- crates/rpc-client/tests/it/http.rs
+++ crates/rpc-client/tests/it/http.rs
@@ -6,7 +6,7 @@ use alloy_rpc_client::{ClientBuilder, RpcCall};
 async fn it_makes_a_request() {
     let anvil = Anvil::new().spawn();
     let url = anvil.endpoint();
-    let client = ClientBuilder::default().reqwest_http(url.parse().unwrap());
+    let client = ClientBuilder::default().http(url.parse().unwrap());
     let req: RpcCall<_, (), U64> = client.request("eth_blockNumber", ());
     let timeout = tokio::time::timeout(std::time::Duration::from_secs(2), req);
     let res = timeout.await.unwrap().unwrap();
