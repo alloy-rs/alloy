e569038b74a4a5a62f0f5ea9e8616c4b6bb97e8b
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-09 21:04:07 +0200

    chore: add helper for next block base fee

--- crates/eips/src/eip1559/basefee.rs
+++ crates/eips/src/eip1559/basefee.rs
@@ -1,5 +1,6 @@
-use crate::eip1559::constants::{
-    DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR, DEFAULT_ELASTICITY_MULTIPLIER,
+use crate::{
+    calc_next_block_base_fee,
+    eip1559::constants::{DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR, DEFAULT_ELASTICITY_MULTIPLIER},
 };
 
 /// BaseFeeParams contains the config parameters that control block base fee computation
@@ -21,5 +22,13 @@ impl BaseFeeParams {
             max_change_denominator: DEFAULT_BASE_FEE_MAX_CHANGE_DENOMINATOR as u128,
             elasticity_multiplier: DEFAULT_ELASTICITY_MULTIPLIER as u128,
         }
+    }
+
+    /// Calculate the base fee for the next block based on the EIP-1559 specification.
+    ///
+    /// See also [calc_next_block_base_fee]
+    #[inline]
+    pub fn next_block_base_fee(self, gas_used: u128, gas_limit: u128, base_fee: u128) -> u128 {
+        calc_next_block_base_fee(gas_used, gas_limit, base_fee, self)
     }
 }
