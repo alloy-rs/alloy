ea5e1c97b81479a3c23a4e0c3b71f50a8b4ce0ec
Matthias Seitz <matthias.seitz@outlook.de>
2024-04-13 08:00:43 +0200

    add docs

--- crates/rpc-types-anvil/src/lib.rs
+++ crates/rpc-types-anvil/src/lib.rs
@@ -6,11 +6,11 @@
 #![warn(
     missing_copy_implementations,
     missing_debug_implementations,
+    missing_docs,
     unreachable_pub,
     clippy::missing_const_for_fn,
     rustdoc::all
 )]
-#![allow(missing_docs)]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![deny(unused_must_use, rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
@@ -19,13 +19,15 @@ use alloy_primitives::{TxHash, B256, U256};
 use serde::{Deserialize, Deserializer, Serialize};
 use std::collections::BTreeMap;
 
-/// Represents the params to set forking which can take various forms
+/// Represents the params to set forking which can take various forms:
 ///  - untagged
 ///  - tagged forking
 #[derive(Clone, Debug, Default, PartialEq, Eq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Forking {
+    /// The URL of the JSON-RPC endpoint to fork from
     pub json_rpc_url: Option<String>,
+    /// The block number to fork from
     pub block_number: Option<u64>,
 }
 
@@ -65,33 +67,50 @@ impl<'de> serde::Deserialize<'de> for Forking {
     }
 }
 
+/// Anvil equivalent of `node_info`.
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NodeInfo {
+    /// The current block number
     #[serde(with = "alloy_serde::u64_hex")]
     pub current_block_number: u64,
+    /// The current block timestamp
     pub current_block_timestamp: u64,
+    /// The current block hash
     pub current_block_hash: B256,
+    /// The enabled hardfork
     pub hard_fork: String,
+    /// How transactions are ordered for mining
     pub transaction_order: String,
+    /// Info about the node's block environment
     pub environment: NodeEnvironment,
+    /// Info about the node's fork configuration
     pub fork_config: NodeForkConfig,
 }
 
+/// The current block environment of the node.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NodeEnvironment {
+    /// Base fee of the current block
     pub base_fee: U256,
+    /// Chain id of the node.
     pub chain_id: u64,
+    /// Configured block gas limit
     pub gas_limit: U256,
+    /// Configured gas price
     pub gas_price: U256,
 }
 
+/// The node's fork configuration.
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct NodeForkConfig {
+    /// URL of the forked network
     pub fork_url: Option<String>,
+    /// Block number of the forked network
     pub fork_block_number: Option<u64>,
+    /// Retry backoff for requests
     pub fork_retry_backoff: Option<u128>,
 }
 
@@ -101,12 +120,19 @@ pub struct NodeForkConfig {
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Metadata {
+    /// client version
     pub client_version: String,
+    /// Chain id of the node.
     pub chain_id: u64,
+    /// Unique instance id
     pub instance_id: B256,
+    /// Latest block number
     pub latest_block_number: u64,
+    /// Latest block hash
     pub latest_block_hash: B256,
+    /// Forked network info
     pub forked_network: Option<ForkedNetwork>,
+    /// Snapshots of the chain
     pub snapshots: BTreeMap<U256, (u64, B256)>,
 }
 
@@ -115,8 +141,11 @@ pub struct Metadata {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ForkedNetwork {
+    /// Chain id of the node.
     pub chain_id: u64,
+    /// Block number of the forked chain
     pub fork_block_number: u64,
+    /// Block hash of the forked chain
     pub fork_block_hash: TxHash,
 }
 
@@ -124,11 +153,13 @@ pub struct ForkedNetwork {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub enum MineOptions {
+    /// The options for mining
     Options {
+        /// The timestamp the block should be mined with
         #[serde(with = "alloy_serde::u64_hex_or_decimal_opt")]
         timestamp: Option<u64>,
-        // If `blocks` is given, it will mine exactly blocks number of blocks, regardless of any
-        // other blocks mined or reverted during it's operation
+        /// If `blocks` is given, it will mine exactly blocks number of blocks, regardless of any
+        /// other blocks mined or reverted during it's operation
         blocks: Option<u64>,
     },
     /// The timestamp the block should be mined with
