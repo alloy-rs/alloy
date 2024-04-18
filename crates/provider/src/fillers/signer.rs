f8861f720a1e3c505c1f52df2cb9b159bb84867b
James <james@prestwi.ch>
2024-04-18 11:42:51 +0100

    fix: some test compilation

--- crates/provider/src/fillers/signer.rs
+++ crates/provider/src/fillers/signer.rs
@@ -97,7 +97,7 @@ mod tests {
         let tx = TransactionRequest {
             nonce: Some(0),
             value: Some(U256::from(100)),
-            to: address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into(),
+            to: Some(address!("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045").into()),
             gas_price: Some(20e9 as u128),
             gas: Some(21000),
             ..Default::default()
