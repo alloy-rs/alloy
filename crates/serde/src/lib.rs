9fedc0d9e21e7d1937fbe4d1d61b67bdf5aaad70
Federico Gimenez <federico.gimenez@gmail.com>
2024-04-25 15:03:43 +0200

    remove extra line

--- crates/serde/src/lib.rs
+++ crates/serde/src/lib.rs
@@ -40,7 +40,6 @@ use serde::Serializer;
 
 use alloy_primitives::B256;
 
-
 /// Serialize a byte vec as a hex string _without_ the "0x" prefix.
 ///
 /// This behaves the same as [`hex::encode`](alloy_primitives::hex::encode).
