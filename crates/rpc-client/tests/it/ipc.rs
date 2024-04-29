da65559a21243397526788c5d2fbd6a265801d78
zerosnacks <zerosnacks@protonmail.com>
2024-04-29 15:45:43 +0000

    use geth port

--- crates/rpc-client/tests/it/ipc.rs
+++ crates/rpc-client/tests/it/ipc.rs
@@ -1,29 +1,22 @@
-use alloy_node_bindings::{Geth, GethInstance};
+use alloy_node_bindings::Geth;
 use alloy_primitives::U64;
-use alloy_pubsub::PubSubFrontend;
-use alloy_rpc_client::{ClientBuilder, RpcCall, RpcClient};
+use alloy_rpc_client::{ClientBuilder, RpcCall};
 use alloy_transport_ipc::IpcConnect;
-use tempfile::TempDir;
-
-async fn connect() -> (RpcClient<PubSubFrontend>, GethInstance) {
-    let temp_dir = TempDir::with_prefix("geth-test-").unwrap();
-    let geth = Geth::new()
-        .disable_discovery()
-        .data_dir(temp_dir.path())
-        .enable_ipc()
-        .block_time(1u64)
-        .spawn();
-    let path = temp_dir.path().join("geth.ipc");
-    let connector: IpcConnect<_> = path.into();
-
-    let client = ClientBuilder::default().pubsub(connector).await.unwrap();
-
-    (client, geth)
-}
 
 #[tokio::test]
 async fn it_makes_a_request() {
-    let (client, _geth) = connect().await;
+    let temp_dir = tempfile::TempDir::with_prefix("geth-test-").unwrap();
+    let _geth = Geth::new()
+        .disable_discovery()
+        .enable_ipc()
+        .block_time(1u64)
+        .data_dir(temp_dir.path())
+        .spawn();
+    let ipc_path = temp_dir.into_path().join("geth.ipc");
+
+    let connector: IpcConnect<_> = ipc_path.into();
+    let client = ClientBuilder::default().pubsub(connector).await.unwrap();
+
     let req: RpcCall<_, (), U64> = client.request("eth_blockNumber", ());
     let timeout = tokio::time::timeout(std::time::Duration::from_secs(2), req);
     let res = timeout.await.unwrap().unwrap();
