c1875d269a8c1b9b2dd4cff3d89fd01b9356f32a
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-06 18:31:03 +0200

    rename

--- crates/rpc-types-beacon/src/lib.rs
+++ crates/rpc-types-beacon/src/lib.rs
@@ -3,9 +3,8 @@
 //! Provides all relevant types for the various RPC endpoints, grouped by namespace.
 
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
-    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
-    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+    html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
+    html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
