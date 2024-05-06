5c75d5fe239ae7abb86b6aae595d0958a686b62c
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-06 17:21:16 +0200

    Update crates/eips/src/eip4844/sidecar.rs
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/eips/src/eip4844/sidecar.rs
+++ crates/eips/src/eip4844/sidecar.rs
@@ -188,7 +188,7 @@ impl Decodable for BlobTransactionSidecar {
 }
 
 // Helper function to deserialize boxed blobs
-#[cfg(feature = "serde")]
+#[cfg(all(debug_assertions, feature = "serde"))]
 fn deserialize_blobs<'de, D>(deserializer: D) -> Result<Vec<Blob>, D::Error>
 where
     D: serde::de::Deserializer<'de>,
