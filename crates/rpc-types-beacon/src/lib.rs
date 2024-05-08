5d698f37c70724f8df0c6dd27e08a39a4bdfc48b
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-05-08 11:12:56 +0300

    feat: re-export rpc-types-beacon in crates/alloy

--- crates/rpc-types-beacon/src/lib.rs
+++ crates/rpc-types-beacon/src/lib.rs
@@ -9,4 +9,19 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-pub mod beacon;
+use alloy_primitives::FixedBytes;
+use constants::{BLS_PUBLIC_KEY_BYTES_LEN, BLS_SIGNATURE_BYTES_LEN};
+
+pub mod constants;
+/// Beacon API events support.
+pub mod events;
+pub mod header;
+pub mod payload;
+pub mod sidecar;
+pub mod withdrawals;
+
+/// BLS signature type
+pub type BlsSignature = FixedBytes<BLS_SIGNATURE_BYTES_LEN>;
+
+/// BLS public key type
+pub type BlsPublicKey = FixedBytes<BLS_PUBLIC_KEY_BYTES_LEN>;
