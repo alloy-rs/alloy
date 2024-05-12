53cafd7512a08fae757d6febc5c440401262bc83
Matthias Seitz <matthias.seitz@outlook.de>
2024-05-12 14:00:08 +0200

    fix link

--- crates/provider/src/ext/debug.rs
+++ crates/provider/src/ext/debug.rs
@@ -37,8 +37,6 @@ pub trait DebugApi<N, T>: Send + Sync {
     ///
     /// # Note
     ///
-    /// https://github.com/ethereum/go-ethereum/blob/44a50c9f96386f44a8682d51cf7500044f6cbaea/eth/tracers/api.go#L448
-    ///
     /// Not all nodes support this call.
     async fn debug_trace_block_by_hash(
         &self,
