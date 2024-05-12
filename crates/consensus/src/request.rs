c2ed9f7dad086d26030e43a5e4bb346cb4fc34b5
Oliver Nordbjerg <hi@notbjerg.me>
2024-05-12 14:56:43 +0300

    not you

--- crates/consensus/src/request.rs
+++ crates/consensus/src/request.rs
@@ -10,10 +10,7 @@ use alloy_rlp::{Decodable, Encodable};
 /// See also [EIP-7685](https://eips.ethereum.org/EIPS/eip-7685).
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 #[non_exhaustive]
-#[cfg_attr(
-    any(test, feature = "arbitrary"),
-    derive(proptest_derive::Arbitrary, arbitrary::Arbitrary)
-)]
+#[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
 pub enum Request {
     /// An [EIP-6110] deposit request.
     ///
