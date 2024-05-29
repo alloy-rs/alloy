45c78c6c1bb022a621fa511d92acd416e9587ed6
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-05-29 08:37:31 -0700

    feat(consensus) implement RLP for Account information (#789)
    * Update account.rs
    
    * fix clippy
    
    * Update account.rs
    
    * Update account.rs
    
    * Update account.rs
    
    * Update account.rs
    
    * Update account.rs
    
    * Update account.rs
    
    * Update account.rs

--- crates/consensus/src/account.rs
+++ crates/consensus/src/account.rs
@@ -1,18 +1,30 @@
-use alloy_primitives::{B256, U256};
+use alloy_primitives::{keccak256, B256, U256};
+use alloy_rlp::{Encodable, RlpDecodable, RlpEncodable};
 
+#[cfg(not(feature = "std"))]
+use alloc::{vec, vec::Vec};
 /// Represents an Account in the account trie.
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, RlpDecodable, RlpEncodable)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(any(test, feature = "arbitrary"), derive(arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct Account {
-    /// The account's balance.
-    pub balance: U256,
-    /// The hash of the code of the account.
-    pub code_hash: B256,
     /// The account's nonce.
     #[cfg_attr(feature = "serde", serde(with = "alloy_serde::u64_via_ruint"))]
     pub nonce: u64,
+    /// The account's balance.
+    pub balance: U256,
     /// The hash of the storage account data.
     pub storage_root: B256,
+    /// The hash of the code of the account.
+    pub code_hash: B256,
+}
+
+impl Account {
+    /// Compute  hash as committed to in the MPT trie without memoizing.
+    pub fn trie_hash_slow(&self) -> B256 {
+        let mut buf = vec![];
+        self.encode(&mut buf);
+        keccak256(buf)
+    }
 }
