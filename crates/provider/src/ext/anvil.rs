a3fe1da4f91fcdfa988dc0ef0353bf1cdc3dc170
zerosnacks <zerosnacks@protonmail.com>
2024-06-06 12:28:22 +0200

    start adding tests

--- crates/provider/src/ext/anvil.rs
+++ crates/provider/src/ext/anvil.rs
@@ -97,30 +97,30 @@ pub trait AnvilApi<N, T>: Send + Sync {
     async fn anvil_remove_pool_transactions(&self, address: Address) -> TransportResult<()>;
 
     /// Snapshot the state of the blockchain at the current block.
-    async fn evm_snapshot(&self) -> TransportResult<U256>;
+    async fn anvil_snapshot(&self) -> TransportResult<U256>;
 
     /// Revert the state of the blockchain to a previous snapshot.
     /// Takes a single parameter, which is the snapshot id to revert to.
-    async fn evm_revert(&self, id: U256) -> TransportResult<bool>;
+    async fn anvil_revert(&self, id: U256) -> TransportResult<bool>;
 
     /// Jump forward in time by the given amount of time, in seconds.
-    async fn evm_increase_time(&self, seconds: U256) -> TransportResult<i64>;
+    async fn anvil_increase_time(&self, seconds: U256) -> TransportResult<i64>;
 
     /// Similar to `evm_increaseTime` but takes the exact timestamp that you want in the next block.
-    async fn evm_set_next_block_timestamp(&self, seconds: u64) -> TransportResult<()>;
+    async fn anvil_set_next_block_timestamp(&self, seconds: u64) -> TransportResult<()>;
 
     /// Sets the specific timestamp and returns the number of seconds between the given timestamp
     /// and the current time.
-    async fn evm_set_time(&self, timestamp: u64) -> TransportResult<u64>;
+    async fn anvil_set_time(&self, timestamp: u64) -> TransportResult<u64>;
 
     /// Set the next block gas limit.
-    async fn evm_set_block_gas_limit(&self, gas_limit: U256) -> TransportResult<bool>;
+    async fn anvil_set_block_gas_limit(&self, gas_limit: U256) -> TransportResult<bool>;
 
     /// Sets an interval for the block timestamp.
-    async fn evm_set_block_timestamp_interval(&self, seconds: u64) -> TransportResult<()>;
+    async fn anvil_set_block_timestamp_interval(&self, seconds: u64) -> TransportResult<()>;
 
     /// Unsets the interval for the block timestamp.
-    async fn evm_remove_block_timestamp_interval(&self) -> TransportResult<bool>;
+    async fn anvil_remove_block_timestamp_interval(&self) -> TransportResult<bool>;
 
     /// Mine blocks, instantly.
     /// This will mine the blocks regardless of the configured mining mode.
@@ -128,7 +128,7 @@ pub trait AnvilApi<N, T>: Send + Sync {
 
     /// Mine blocks, instantly and return the mined blocks.
     /// This will mine the blocks regardless of the configured mining mode.
-    async fn evm_mine_detailed(&self, opts: Option<MineOptions>) -> TransportResult<Vec<Block>>;
+    async fn anvil_mine_detailed(&self, opts: Option<MineOptions>) -> TransportResult<Vec<Block>>;
 
     /// Sets the reported block number.
     async fn anvil_set_block(&self, block_number: U256) -> TransportResult<()>;
@@ -225,7 +225,7 @@ where
     }
 
     async fn anvil_set_logging(&self, enable: bool) -> TransportResult<()> {
-        self.client().request("anvil_setLogging", (enable,)).await
+        self.client().request("anvil_setLoggingEnabled", (enable,)).await
     }
 
     async fn anvil_set_min_gas_price(&self, gas: U256) -> TransportResult<()> {
@@ -260,44 +260,44 @@ where
         self.client().request("anvil_removePoolTransactions", (address,)).await
     }
 
-    async fn evm_snapshot(&self) -> TransportResult<U256> {
+    async fn anvil_snapshot(&self) -> TransportResult<U256> {
         self.client().request("evm_snapshot", ()).await
     }
 
-    async fn evm_revert(&self, id: U256) -> TransportResult<bool> {
+    async fn anvil_revert(&self, id: U256) -> TransportResult<bool> {
         self.client().request("evm_revert", (id,)).await
     }
 
-    async fn evm_increase_time(&self, seconds: U256) -> TransportResult<i64> {
+    async fn anvil_increase_time(&self, seconds: U256) -> TransportResult<i64> {
         self.client().request("evm_increaseTime", (seconds,)).await
     }
 
-    async fn evm_set_next_block_timestamp(&self, seconds: u64) -> TransportResult<()> {
+    async fn anvil_set_next_block_timestamp(&self, seconds: u64) -> TransportResult<()> {
         self.client().request("evm_setNextBlockTimestamp", (seconds,)).await
     }
 
-    async fn evm_set_time(&self, timestamp: u64) -> TransportResult<u64> {
+    async fn anvil_set_time(&self, timestamp: u64) -> TransportResult<u64> {
         self.client().request("evm_setTime", (timestamp,)).await
     }
 
-    async fn evm_set_block_gas_limit(&self, gas_limit: U256) -> TransportResult<bool> {
+    async fn anvil_set_block_gas_limit(&self, gas_limit: U256) -> TransportResult<bool> {
         self.client().request("evm_setBlockGasLimit", (gas_limit,)).await
     }
 
-    async fn evm_set_block_timestamp_interval(&self, seconds: u64) -> TransportResult<()> {
-        self.client().request("evm_setBlockTimestampInterval", (seconds,)).await
+    async fn anvil_set_block_timestamp_interval(&self, seconds: u64) -> TransportResult<()> {
+        self.client().request("anvil_setBlockTimestampInterval", (seconds,)).await
     }
 
-    async fn evm_remove_block_timestamp_interval(&self) -> TransportResult<bool> {
-        self.client().request("evm_removeBlockTimestampInterval", ()).await
+    async fn anvil_remove_block_timestamp_interval(&self) -> TransportResult<bool> {
+        self.client().request("anvil_removeBlockTimestampInterval", ()).await
     }
 
     async fn evm_mine(&self, opts: Option<MineOptions>) -> TransportResult<String> {
         self.client().request("evm_mine", (opts,)).await
     }
 
-    async fn evm_mine_detailed(&self, opts: Option<MineOptions>) -> TransportResult<Vec<Block>> {
-        self.client().request("evm_mineDetailed", (opts,)).await
+    async fn anvil_mine_detailed(&self, opts: Option<MineOptions>) -> TransportResult<Vec<Block>> {
+        self.client().request("evm_mine_detailed", (opts,)).await
     }
 
     async fn anvil_set_block(&self, block_number: U256) -> TransportResult<()> {
@@ -318,4 +318,241 @@ where
     ) -> TransportResult<TxHash> {
         self.client().request("eth_sendUnsignedTransaction", (request,)).await
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloy_eips::BlockNumberOrTag;
+    use alloy_network::TransactionBuilder;
+
+    use crate::ProviderBuilder;
+
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_impersonate_account_anvil_stop_impersonating_account() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let impersonate = Address::random();
+        let to = Address::random();
+        let val = U256::from(1337);
+        let funding = U256::from(1e18 as u64);
+
+        // Fund the impersonated account.
+        provider.anvil_set_balance(impersonate, funding).await.unwrap();
+
+        let balance = provider.get_balance(impersonate).await.unwrap();
+        assert_eq!(balance, funding);
+
+        let tx = TransactionRequest::default().with_from(impersonate).with_to(to).with_value(val);
+
+        let res = provider.send_transaction(tx.clone()).await;
+        res.unwrap_err();
+
+        provider.anvil_impersonate_account(impersonate).await.unwrap();
+        assert!(provider.get_accounts().await.unwrap().contains(&impersonate));
+
+        let res = provider.send_transaction(tx.clone()).await.unwrap().get_receipt().await.unwrap();
+        assert_eq!(res.from, impersonate);
+
+        let nonce = provider.get_transaction_count(impersonate).await.unwrap();
+        assert_eq!(nonce, 1);
+
+        let balance = provider.get_balance(to).await.unwrap();
+        assert_eq!(balance, val);
+
+        provider.anvil_stop_impersonating_account(impersonate).await.unwrap();
+        let res = provider.send_transaction(tx).await;
+        res.unwrap_err();
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_auto_impersonate_account() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let impersonate = Address::random();
+        let to = Address::random();
+        let val = U256::from(1337);
+        let funding = U256::from(1e18 as u64);
+
+        // Fund the impersonated account.
+        provider.anvil_set_balance(impersonate, funding).await.unwrap();
+
+        let balance = provider.get_balance(impersonate).await.unwrap();
+        assert_eq!(balance, funding);
+
+        let tx = TransactionRequest::default().with_from(impersonate).with_to(to).with_value(val);
+
+        let res = provider.send_transaction(tx.clone()).await;
+        res.unwrap_err();
+
+        provider.anvil_auto_impersonate_account(true).await.unwrap();
+
+        let res = provider.send_transaction(tx.clone()).await.unwrap().get_receipt().await.unwrap();
+        assert_eq!(res.from, impersonate);
+
+        let nonce = provider.get_transaction_count(impersonate).await.unwrap();
+        assert_eq!(nonce, 1);
+
+        let balance = provider.get_balance(to).await.unwrap();
+        assert_eq!(balance, val);
+
+        provider.anvil_auto_impersonate_account(false).await.unwrap();
+        let res = provider.send_transaction(tx).await;
+        res.unwrap_err();
+
+        // Explicitly impersonated accounts get returned by `eth_accounts`
+        provider.anvil_impersonate_account(impersonate).await.unwrap();
+        assert!(provider.get_accounts().await.unwrap().contains(&impersonate));
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_get_auto_mine() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_auto_mine() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_mine() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_interval_mining() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_drop_transaction() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_drop_all_transactions() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_reset() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_chain_id() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_balance() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_code() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let address = Address::with_last_byte(16);
+        provider.anvil_set_code(address, Bytes::from("0xbeef")).await.unwrap();
+
+        let code = provider.get_code_at(address).await.unwrap();
+        assert_eq!(code, Bytes::from("0xbeef"));
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_nonce() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_storage_at() {
+        // let provider = ProviderBuilder::new().on_anvil();
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_logging() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_min_gas_price() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_next_block_base_fee_per_gas() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_coinbase() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let coinbase = Address::random();
+        provider.anvil_set_coinbase(coinbase).await.unwrap();
+
+        // Mine a new block, and check the new block coinbase.
+        let _ = provider.evm_mine(None).await;
+
+        let block =
+            provider.get_block_by_number(BlockNumberOrTag::Latest, false).await.unwrap().unwrap();
+        assert_eq!(block.header.miner, coinbase);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_dump_state() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_load_state() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_node_info() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_metadata() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_remove_pool_transactions() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_snapshot() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_revert() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_increase_time() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_next_block_timestamp() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_time() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_block_gas_limit() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let block_gas_limit = U256::from(1337);
+        assert!(provider.anvil_set_block_gas_limit(block_gas_limit).await.unwrap());
+
+        // Mine a new block, and check the new block gas limit.
+        let _ = provider.evm_mine(None).await;
+
+        let latest_block =
+            provider.get_block_by_number(BlockNumberOrTag::Latest, false).await.unwrap().unwrap();
+        assert_eq!(block_gas_limit.to::<u128>(), latest_block.header.gas_limit);
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_block_timestamp_interval() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_remove_block_timestamp_interval() {}
+
+    // Tests: evm_mine
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_evm_mine() {
+        let provider = ProviderBuilder::new().on_anvil();
+
+        let start_num = provider.get_block_number().await.unwrap();
+
+        for (idx, _) in std::iter::repeat(()).take(10).enumerate() {
+            provider.evm_mine(None).await.unwrap();
+            let num = provider.get_block_number().await.unwrap();
+            assert_eq!(num, start_num + idx as u64 + 1);
+        }
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_mine_detailed() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_block() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_set_rpc_url() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_anvil_enable_traces() {}
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_eth_send_unsigned_transaction() {}
 }
