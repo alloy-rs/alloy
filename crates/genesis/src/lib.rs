3b6c6ce2483a6d8aac60922deb766e7a29759609
zerosnacks <zerosnacks@protonmail.com>
2024-06-11 13:56:59 +0200

    fix clippy

--- crates/genesis/src/lib.rs
+++ crates/genesis/src/lib.rs
@@ -12,7 +12,7 @@
 extern crate alloc;
 
 use alloc::{collections::BTreeMap, string::String};
-use alloy_primitives::{Address, Bytes, ChainId, B256, U256};
+use alloy_primitives::{Address, Bytes, B256, U256};
 use alloy_serde::{storage::deserialize_storage_map, ttd::deserialize_json_ttd_opt};
 use serde::{Deserialize, Serialize};
 
