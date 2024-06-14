10c7e45a3bb78a331ac5f6de2a1061da4dc068e7
zerosnacks <zerosnacks@protonmail.com>
2024-06-14 18:54:14 +0200

    revert unintended changes, leftover

--- crates/rpc-types-beacon/src/lib.rs
+++ crates/rpc-types-beacon/src/lib.rs
@@ -2,13 +2,12 @@
 //!
 //! Provides all relevant types for the various RPC endpoints, grouped by namespace.
 
-#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 use alloy_primitives::FixedBytes;
 use constants::{BLS_PUBLIC_KEY_BYTES_LEN, BLS_SIGNATURE_BYTES_LEN};
