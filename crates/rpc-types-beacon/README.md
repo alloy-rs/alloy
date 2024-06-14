7ef249b5f3f261886708a1b7e6068f9acee604bb
zerosnacks <zerosnacks@protonmail.com>
2024-06-14 18:56:34 +0200

    apply suggestions

--- crates/rpc-types-beacon/README.md
+++ crates/rpc-types-beacon/README.md
@@ -1,3 +1,3 @@
 # alloy-rpc-types-beacon
 
-Ethereum RPC-related types for the Beacon API.
+Ethereum RPC-related types for the [Beacon API](https://ethereum.github.io/beacon-APIs).
--- crates/rpc-types-beacon/src/lib.rs
+++ crates/rpc-types-beacon/src/lib.rs
@@ -1,13 +1,10 @@
-//! [Beacon API](https://ethereum.github.io/beacon-APIs) types
-//!
-//! Provides all relevant types for the various RPC endpoints, grouped by namespace.
-
+#![doc = include_str!("../README.md")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 
 use alloy_primitives::FixedBytes;
 use constants::{BLS_PUBLIC_KEY_BYTES_LEN, BLS_SIGNATURE_BYTES_LEN};
