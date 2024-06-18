f89bfb809e0bf6e0d6adbc1798834bf97c6f34b8
Matthias Seitz <matthias.seitz@outlook.de>
2024-06-18 18:35:07 +0200

    docs: update get_balance docs

--- crates/provider/src/provider/trait.rs
+++ crates/provider/src/provider/trait.rs
@@ -248,7 +248,9 @@ pub trait Provider<T: Transport + Clone = BoxTransport, N: Network = Ethereum>:
         RpcWithBlock::new(self.weak_client(), "eth_getAccount", address)
     }
 
-    /// Gets the balance of the account at the specified tag, which defaults to latest.
+    /// Gets the balance of the account.
+    ///
+    /// Defaults to the latest block. See also [RpcWithBlock::block_id].
     fn get_balance(&self, address: Address) -> RpcWithBlock<T, Address, U256> {
         RpcWithBlock::new(self.weak_client(), "eth_getBalance", address)
     }
