d3177ba859e09e34a5e57d96450c7a978943abcc
Oliver Nordbjerg <hi@notbjerg.me>
2024-06-18 14:52:56 +0200

    chore: update blockhash history address

--- crates/eips/src/eip2935.rs
+++ crates/eips/src/eip2935.rs
@@ -5,7 +5,7 @@
 use alloy_primitives::{address, bytes, Address, Bytes};
 
 /// The address for the EIP-2935 history storage contract.
-pub const HISTORY_STORAGE_ADDRESS: Address = address!("25a219378dad9b3503c8268c9ca836a52427a4fb");
+pub const HISTORY_STORAGE_ADDRESS: Address = address!("0aae40965e6800cd9b1f4b05ff21581047e3f91e");
 
 /// The code for the EIP-2935 history storage contract.
 pub static HISTORY_STORAGE_CODE: Bytes = bytes!("3373fffffffffffffffffffffffffffffffffffffffe1460575767ffffffffffffffff5f3511605357600143035f3511604b575f35612000014311604b57611fff5f3516545f5260205ff35b5f5f5260205ff35b5f5ffd5b5f35611fff60014303165500");
