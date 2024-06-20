c99587f345edb019751e74e8799a34bdb3b9b5e9
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-06-20 14:46:33 +0200

    chore: unused crate deps

--- crates/eips/src/lib.rs
+++ crates/eips/src/lib.rs
@@ -3,6 +3,7 @@
     html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
