f33ce645b4a08876ea65393e419a9f1b417e639e
Wodann <Wodann@users.noreply.github.com>
2024-06-21 18:58:27 +0000

    misc: apply review suggestions

--- crates/serde/src/optional.rs
+++ crates/serde/src/optional.rs
@@ -1,17 +1,9 @@
 //! Serde functions for encoding optional values.
-//!
-//! This is defined as a "hex encoded unsigned integer", with a special case of 0 being `0x0`.
-//!
-//! A regex for this format is: `^0x([1-9a-f]+[0-9a-f]*|0)$`.
-//!
-//! This is only valid for human-readable [`serde`] implementations.
-//! For non-human-readable implementations, the format is unspecified.
-//! Currently, it uses a fixed-width big-endian byte-array.
 use serde::{Deserialize, Deserializer};
 
 /// For use with serde's `deserialize_with` on a sequence that must be
 /// deserialized as a single but optional (i.e. possibly `null`) value.
-pub fn or_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+pub fn null_as_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
 where
     T: Deserialize<'de> + Default,
     D: Deserializer<'de>,
