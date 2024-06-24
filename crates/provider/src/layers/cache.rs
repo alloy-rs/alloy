df7d861316878e0369aecc838f1f8273456660ba
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-06-24 13:21:37 +0530

    load and dump cache from fs

--- crates/provider/src/layers/cache.rs
+++ crates/provider/src/layers/cache.rs
@@ -5,9 +5,9 @@ use alloy_primitives::B256;
 use alloy_rpc_types_eth::{Block, BlockNumberOrTag};
 use alloy_transport::{Transport, TransportErrorKind, TransportResult};
 use lru::LruCache;
-use std::{marker::PhantomData, num::NonZeroUsize, path::PathBuf};
+use serde::{Deserialize, Serialize};
+use std::{io::BufReader, marker::PhantomData, num::NonZeroUsize, path::PathBuf};
 use tokio::sync::Mutex;
-
 // TODO: Populate load cache from file on initialization.
 // TODO: Add method to dump cache to file.
 /// A provider layer that caches RPC responses and serves them on subsequent requests.
@@ -85,6 +85,36 @@ where
         let val = cache.get(key).cloned();
         Ok(val)
     }
+
+    pub async fn dump_cache(&self) -> TransportResult<()> {
+        let cache = self.cache.lock().await;
+        let file = std::fs::File::create(self.path.clone()).map_err(TransportErrorKind::custom)?;
+
+        // Iterate over the cache and dump to the file.
+        let entries = cache
+            .iter()
+            .map(|(key, value)| FsCacheEntry { key: key.clone(), value: value.clone() })
+            .collect::<Vec<_>>();
+        serde_json::to_writer(file, &entries).map_err(TransportErrorKind::custom)?;
+        Ok(())
+    }
+
+    pub async fn load_cache(&self) -> TransportResult<()> {
+        if !self.path.exists() {
+            return Ok(());
+        };
+        let file = std::fs::File::open(self.path.clone()).map_err(TransportErrorKind::custom)?;
+        let file = BufReader::new(file);
+        let entries: Vec<FsCacheEntry> =
+            serde_json::from_reader(file).map_err(TransportErrorKind::custom)?;
+        let mut cache = self.cache.lock().await;
+        for entry in entries {
+            cache.put(entry.key, entry.value);
+        }
+
+        println!("Loaded {} cache entries", cache.len());
+        Ok(())
+    }
 }
 
 #[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
@@ -119,7 +149,7 @@ where
         println!("Returned from RPC!");
         if let Some(ref block) = rpc_res {
             let json_str = serde_json::to_string(block).map_err(TransportErrorKind::custom)?;
-            let old_val = self.put(hash, json_str).await?;
+            let _old_val = self.put(hash, json_str).await?;
         }
 
         Ok(rpc_res)
@@ -129,8 +159,18 @@ where
     // eth_getStorage etc.
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct FsCacheEntry {
+    /// Hash of the request params
+    key: B256,
+    /// Serialized response to the request from which the hash was computed.
+    value: String,
+}
+
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use crate::ProviderBuilder;
     use alloy_node_bindings::Anvil;
 
@@ -138,13 +178,21 @@ mod tests {
 
     #[tokio::test]
     async fn test_cache_provider() {
-        let cache = CacheLayer::new(PathBuf::new(), 100);
-        let anvil = Anvil::new().spawn();
+        let cache = CacheLayer::new(PathBuf::from_str("./rpc-cache.txt").unwrap(), 100);
+        let anvil = Anvil::new().block_time_f64(0.3).spawn();
         let provider = ProviderBuilder::default().layer(cache).on_http(anvil.endpoint_url());
 
+        provider.load_cache().await.unwrap();
         let blk = provider.get_block_by_number(0.into(), true).await.unwrap();
         let blk2 = provider.get_block_by_number(0.into(), true).await.unwrap();
 
+        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
+        let latest_block_num = provider.get_block_number().await.unwrap();
+        let blk3 = provider.get_block_by_number(latest_block_num.into(), true).await.unwrap();
+        let blk4 = provider.get_block_by_number(latest_block_num.into(), true).await.unwrap();
+
+        provider.dump_cache().await.unwrap();
         assert_eq!(blk, blk2);
+        assert_eq!(blk3, blk4);
     }
 }
