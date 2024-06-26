a48e71a18b48d60942e7d7fc84489f9ddc29a608
jn <nxqd.inbox+rhacker@gmail.com>
2024-06-26 01:26:04 -0700

    dev(hotfix): typo change pub(crate) to pub (#979)

--- crates/rpc-types-mev/src/eth_calls.rs
+++ crates/rpc-types-mev/src/eth_calls.rs
@@ -108,32 +108,32 @@ pub struct CancelPrivateTransactionRequest {
 /// <https://docs.flashbots.net/flashbots-auction/searchers/advanced/rpc-endpoint#eth_sendbundle>
 #[derive(Default, Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub(crate) struct EthSendBundle {
+pub struct EthSendBundle {
     /// A list of hex-encoded signed transactions
-    pub(crate) txs: Vec<Bytes>,
+    pub txs: Vec<Bytes>,
     /// hex-encoded block number for which this bundle is valid
     #[serde(with = "alloy_serde::quantity")]
-    pub(crate) block_number: u64,
+    pub block_number: u64,
     /// unix timestamp when this bundle becomes active
     #[serde(default, with = "alloy_serde::quantity::opt", skip_serializing_if = "Option::is_none")]
-    pub(crate) min_timestamp: Option<u64>,
+    pub min_timestamp: Option<u64>,
     /// unix timestamp how long this bundle stays valid
     #[serde(default, with = "alloy_serde::quantity::opt", skip_serializing_if = "Option::is_none")]
-    pub(crate) max_timestamp: Option<u64>,
+    pub max_timestamp: Option<u64>,
     /// list of hashes of possibly reverting txs
     #[serde(default, skip_serializing_if = "Vec::is_empty")]
-    pub(crate) reverting_tx_hashes: Vec<B256>,
+    pub reverting_tx_hashes: Vec<B256>,
     /// UUID that can be used to cancel/replace this bundle
     #[serde(default, rename = "replacementUuid", skip_serializing_if = "Option::is_none")]
-    pub(crate) replacement_uuid: Option<String>,
+    pub replacement_uuid: Option<String>,
 }
 
 /// Response from the matchmaker after sending a bundle.
 #[derive(Deserialize, Debug, Serialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
-pub(crate) struct EthBundleHash {
+pub struct EthBundleHash {
     /// Hash of the bundle bodies.
-    pub(crate) bundle_hash: B256,
+    pub bundle_hash: B256,
 }
 
 /// Response from the matchmaker after sending a bundle.
