alloy-rs/alloy

commit 194f266fae46c1214004d04d4606010c3807b534

fix: deserialization of null storage keys in AccessListItem (#955)
* fix: deserialization of null storage keys in AccessListItem

* test: add test

* misc: apply review suggestions

* Update crates/eips/src/eip2930.rs

Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

* misc: apply review suggestion

---------

Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

crates/eips/src/eip2930.rs
@@ -30,6 +30,8 @@ pub struct AccessListItem {
             strategy = "proptest::collection::vec(proptest::arbitrary::any::<B256>(), 0..=20)"
         )
     )]
+    // In JSON, we have to accept `null` for storage key, which is interpreted as an empty array.
+    #[cfg_attr(feature = "serde", serde(deserialize_with = "alloy_serde::null_as_default"))]
     pub storage_keys: Vec<B256>,
 }
 
@@ -166,7 +168,23 @@ pub struct AccessListWithGasUsed {
 
 #[cfg(all(test, feature = "serde"))]
 mod tests {
+    use serde_json::json;
+
     use super::*;
+
+    #[test]
+    fn access_list_null_storage_keys() {
+        let json = json!([
+            {
+                "address": "0x81b7bdd5b89c90b63f604fc7cdd17035cb939707",
+                "storageKeys": null,
+            }
+        ]);
+
+        let access_list = serde_json::from_value::<AccessList>(json).unwrap();
+        assert_eq!(access_list.len(), 1);
+        assert_eq!(access_list[0].storage_keys, Vec::<B256>::default());
+    }
 
     #[test]
     fn access_list_serde() {

crates/serde/src/lib.rs
@@ -18,6 +18,9 @@ use serde::Serializer;
 mod bool;
 pub use self::bool::*;
 
+mod optional;
+pub use self::optional::*;
+
 #[cfg_attr(not(test), deprecated = "use `quantity::{self, opt, vec}` instead")]
 pub mod num;
 #[allow(deprecated)]

crates/serde/src/optional.rs
@@ -1,0 +1,13 @@
+//! Serde functions for encoding optional values.
+use serde::{Deserialize, Deserializer};
+
+/// For use with serde's `deserialize_with` on a sequence that must be
+/// deserialized as a single but optional (i.e. possibly `null`) value.
+pub fn null_as_default<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de> + Default,
+    D: Deserializer<'de>,
+{
+    let s: Option<T> = Deserialize::deserialize(deserializer)?;
+    Ok(s.unwrap_or_default())
+}

commit 1d0c81dcdc522b77c392c92c0814f41413011af2

fix: require storageKeys value broken bincode serialization from #955 (#1058)
* fix: broken bincode serialization from #955

* remove optional in alloy-serde

* resolve matt's comments

* get rid of broken test

* fix cargo fmt

* enforce storage key

---------

Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

crates/eips/src/eip2930.rs
@@ -8,7 +8,6 @@ use alloc::vec::Vec;
 use alloy_primitives::{Address, B256, U256};
 use alloy_rlp::{RlpDecodable, RlpDecodableWrapper, RlpEncodable, RlpEncodableWrapper};
 use core::{mem, ops::Deref};
-
 /// A list of addresses and storage keys that the transaction plans to access.
 /// Accesses outside the list are possible, but become more expensive.
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash, RlpDecodable, RlpEncodable)]
@@ -19,8 +18,6 @@ pub struct AccessListItem {
     /// Account addresses that would be loaded at the start of execution
     pub address: Address,
     /// Keys of storage that would be loaded at the start of execution
-    // In JSON, we have to accept `null` for storage key, which is interpreted as an empty array.
-    #[cfg_attr(feature = "serde", serde(deserialize_with = "alloy_serde::null_as_default"))]
     pub storage_keys: Vec<B256>,
 }
 
@@ -146,23 +143,7 @@ pub struct AccessListWithGasUsed {
 
 #[cfg(all(test, feature = "serde"))]
 mod tests {
-    use serde_json::json;
-
     use super::*;
-
-    #[test]
-    fn access_list_null_storage_keys() {
-        let json = json!([
-            {
-                "address": "0x81b7bdd5b89c90b63f604fc7cdd17035cb939707",
-                "storageKeys": null,
-            }
-        ]);
-
-        let access_list = serde_json::from_value::<AccessList>(json).unwrap();
-        assert_eq!(access_list.len(), 1);
-        assert_eq!(access_list[0].storage_keys, Vec::<B256>::default());
-    }
 
     #[test]
     fn access_list_serde() {