2448e2a01d5b0807ae2c3167e1ece110e47647a4
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-07-20 07:56:52 -0700

    Update debug.rs

--- crates/provider/src/ext/debug.rs
+++ crates/provider/src/ext/debug.rs
@@ -2,7 +2,10 @@
 use crate::Provider;
 use alloy_network::Network;
 use alloy_primitives::{hex, Bytes, TxHash, B256};
-use alloy_rpc_types_eth::{Block, BlockNumberOrTag, TransactionRequest};
+use alloy_rpc_types_eth::{
+    state::StateOverride, Block, BlockNumberOrTag, EthCallResponse, StateContext,
+    TransactionRequest,
+};
 use alloy_rpc_types_trace::geth::{
     BlockTraceResult, GethDebugTracingCallOptions, GethDebugTracingOptions, GethTrace, TraceResult,
 };
@@ -126,6 +129,15 @@ pub trait DebugApi<N, T>: Send + Sync {
         block: BlockNumberOrTag,
         trace_options: GethDebugTracingCallOptions,
     ) -> TransportResult<Vec<GethTrace>>;
+
+    /// Simulate arbitrary number of transactions at an arbitrary blockchain index, with the
+    /// optionality of state overrides
+    async fn call_many(
+        &self,
+        tx: TransactionRequest,
+        state_context: Option<StateContext>,
+        state_override: Option<StateOverride>,
+    ) -> TransportResult<Vec<EthCallResponse>>;
 }
 
 #[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
@@ -136,6 +148,14 @@ where
     T: Transport + Clone,
     P: Provider<T, N>,
 {
+    async fn call_many(
+        &self,
+        tx: TransactionRequest,
+        state_context: Option<StateContext>,
+        state_override: Option<StateOverride>,
+    ) -> TransportResult<Vec<EthCallResponse>> {
+        self.client().request("callMany", (tx, state_context, state_override)).await
+    }
     async fn debug_get_raw_header(&self, block: BlockNumberOrTag) -> TransportResult<Bytes> {
         self.client().request("debug_getRawHeader", (block,)).await
     }
