eb5f0b3821dcbc5cd22231a44ce1312ba1f70c2d
Matthias Seitz <matthias.seitz@outlook.de>
2024-07-25 09:58:25 +0200

    touchups

--- crates/rpc-types-eth/src/simulate.rs
+++ crates/rpc-types-eth/src/simulate.rs
@@ -1,6 +1,6 @@
-//! 'eth_simulateV1' Request / Response types
+//! 'eth_simulateV1' Request / Response types: <https://github.com/ethereum/execution-apis/pull/484>
 
-use alloy_primitives::{Address, Bytes, Log, B256, U64};
+use alloy_primitives::{Address, Bytes, Log, B256};
 use serde::{Deserialize, Serialize};
 
 use crate::{state::StateOverride, BlockOverrides, TransactionRequest};
@@ -26,19 +26,24 @@ pub struct SimBlock {
 #[serde(rename_all = "camelCase")]
 pub struct SimulatedBlock {
     /// The number of the block.
-    pub number: U64,
+    #[serde(with = "alloy_serde::quantity")]
+    pub number: u64,
     /// The hash of the block.
     pub hash: B256,
     /// The timestamp of the block.
-    pub timestamp: U64,
+    #[serde(with = "alloy_serde::quantity")]
+    pub timestamp: u64,
     /// The gas limit of the block.
-    pub gas_limit: U64,
+    #[serde(with = "alloy_serde::quantity")]
+    pub gas_limit: u64,
     /// The amount of gas used in the block.
-    pub gas_used: U64,
+    #[serde(with = "alloy_serde::quantity")]
+    pub gas_used: u64,
     /// The recipient of the block's fees.
     pub fee_recipient: Address,
     /// The base fee per gas unit for the block.
-    pub base_fee_per_gas: U64,
+    #[serde(with = "alloy_serde::quantity")]
+    pub base_fee_per_gas: u64,
     /// The previous RANDAO value of the block.
     pub prev_randao: B256,
     /// A vector of results for each call in the block.
@@ -59,12 +64,17 @@ pub struct SimCallResult {
     /// The raw bytes returned by the transaction.
     pub return_value: Bytes,
     /// Logs generated during the execution of the transaction.
+    #[serde(default)]
     pub logs: Vec<Log>,
     /// The amount of gas used by the transaction.
-    pub gas_used: U64,
     #[serde(with = "alloy_serde::quantity")]
+    pub gas_used: u64,
     /// The final status of the transaction, typically indicating success or failure.
+    #[serde(with = "alloy_serde::quantity")]
     pub status: u64,
+    /// Error in case the call failed
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub error: Option<SimulateError>,
 }
 
 /// Simulation options for executing multiple blocks and transactions.
@@ -72,29 +82,35 @@ pub struct SimCallResult {
 /// validate transaction sequences, and whether to return full transaction objects.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct SimOpts {
-    /// A vector of simulated blocks each containing state and transaction overrides.
+pub struct SimulatePayload {
+    /// Array of block state calls to be executed at specific, optional block/state.
     pub block_state_calls: Vec<SimBlock>,
     /// Flag to determine whether to trace ERC20/ERC721 token transfers within transactions.
+    #[serde(default)]
     pub trace_transfers: bool,
     /// Flag to enable or disable validation of the transaction sequence in the blocks.
+    #[serde(default)]
     pub validation: bool,
-    /// Flag to decide if full transactions should be returned instead of just their outcomes.
+    /// Flag to decide if full transactions should be returned instead of just their hashes.
     pub return_full_transactions: bool,
 }
+
 /// The error response returned by the `eth_simulateV1` method.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct SimulateV1Error {
-    /// code error
-    code: i32,
+pub struct SimulateError {
+    /// Code error
+    /// -3200: Execution reverted
+    /// -32015: VM execution error
+    pub code: i32,
     /// Message error
-    message: String,
+    pub message: String,
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use alloy_primitives::{Address, TxKind};
+    use alloy_primitives::{Address, TxKind, U64};
     use serde_json::json;
 
     #[test]
@@ -141,32 +157,24 @@ mod tests {
             }, "latest"]
         });
 
-        let error_response_json = json!({
-            "jsonrpc": "2.0",
-            "id": 1,
-            "error": {
-                "code": -32000,
-                "message": "account 0xC000000000000000000000000000000000000000 is not a precompile"
-            }
-        });
-
-        let sim_opts: SimOpts = serde_json::from_value(request_json["params"][0].clone()).unwrap();
+        let sim_opts: SimulatePayload =
+            serde_json::from_value(request_json["params"][0].clone()).unwrap();
 
         let address_1: Address = "0xc000000000000000000000000000000000000000".parse().unwrap();
         let address_2: Address = "0xc100000000000000000000000000000000000000".parse().unwrap();
 
-        assert_eq!(sim_opts.validation, true);
+        assert!(sim_opts.validation);
         assert_eq!(sim_opts.block_state_calls.len(), 2);
 
         let block_state_call_1 = &sim_opts.block_state_calls[0];
-        assert!(block_state_call_1.state_overrides.get(&address_1).is_some());
+        assert!(block_state_call_1.state_overrides.contains_key(&address_1));
         assert_eq!(
             block_state_call_1.state_overrides.get(&address_1).unwrap().nonce.unwrap(),
             U64::from(5)
         );
 
         let block_state_call_2 = &sim_opts.block_state_calls[1];
-        assert!(block_state_call_2.state_overrides.get(&address_1).is_some());
+        assert!(block_state_call_2.state_overrides.contains_key(&address_1));
 
         assert_eq!(block_state_call_2.calls.len(), 2);
         assert_eq!(block_state_call_2.calls[0].from.unwrap(), address_1);
@@ -175,14 +183,5 @@ mod tests {
         assert_eq!(block_state_call_2.calls[1].from.unwrap(), address_2);
         assert_eq!(block_state_call_2.calls[1].to.unwrap(), TxKind::Call(address_2));
         assert_eq!(block_state_call_2.calls[1].nonce.unwrap(), 5);
-
-        let error_response: SimulateV1Error =
-            serde_json::from_value(error_response_json["error"].clone()).unwrap();
-
-        assert_eq!(error_response.code, -32000);
-        assert_eq!(
-            error_response.message,
-            "account 0xC000000000000000000000000000000000000000 is not a precompile"
-        );
     }
 }
