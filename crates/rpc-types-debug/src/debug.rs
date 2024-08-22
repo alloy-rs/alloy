bd6f02c6bfe792635154c1e9638b38910d4cfb40
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-08-22 18:42:46 +0100

    clarify unhashed

--- crates/rpc-types-debug/src/debug.rs
+++ crates/rpc-types-debug/src/debug.rs
@@ -10,7 +10,8 @@ pub struct ExecutionWitness {
     /// Map of all hashed trie nodes to their preimages that were required during the execution of
     /// the block, including during state root recomputation.
     pub witness: HashMap<B256, Bytes>,
-    /// Map of all hashed account addresses and storage slots to their preimages that were required
-    /// during the execution of the block.
+    /// Map of all hashed account addresses and storage slots to their preimages (unhashed account
+    /// addresses and storage slots, respectively) that were required during the execution of the
+    /// block. during the execution of the block.
     pub state_preimages: Option<HashMap<B256, Bytes>>,
 }
