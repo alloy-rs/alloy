cecf1b8e6f476d44d601816c17780c7fb8dc2192
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-08-26 13:59:23 +0200

    fix(provider): serialize no parameters as `[]` instead of `null` (#1193)
    * fix(provider): replace unit with empty array in `ChainStreamPoller`
    
    * fix: everywhere
    
    * doctest
    
    * alias
    
    * simpler

--- crates/rpc-client/tests/it/ws.rs
+++ crates/rpc-client/tests/it/ws.rs
@@ -9,7 +9,7 @@ async fn it_makes_a_request() {
     let url = anvil.ws_endpoint();
     let connector = WsConnect { url: url.parse().unwrap(), auth: None };
     let client = ClientBuilder::default().pubsub(connector).await.unwrap();
-    let req: RpcCall<_, (), U64> = client.request("eth_blockNumber", ());
+    let req: RpcCall<_, _, U64> = client.request_noparams("eth_blockNumber");
     let timeout = tokio::time::timeout(std::time::Duration::from_secs(2), req);
     let res = timeout.await.unwrap().unwrap();
     assert_eq!(res.to::<u64>(), 0);
