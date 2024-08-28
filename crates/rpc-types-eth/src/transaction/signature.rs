723db63d5c0442f8b30d8e2ff0c8be86bca823fc
Emilia Hane <elsaemiliaevahane@gmail.com>
2024-08-28 10:06:29 +0200

    Simplify y-parity conversion

--- crates/rpc-types-eth/src/transaction/signature.rs
+++ crates/rpc-types-eth/src/transaction/signature.rs
@@ -81,10 +81,7 @@ impl From<alloy_primitives::Signature> for Signature {
             v: U256::from(signature.v().to_u64()),
             r: signature.r(),
             s: signature.s(),
-            y_parity: Some(Parity::from(
-                signature.v().y_parity_byte_non_eip155().unwrap_or(signature.v().y_parity_byte())
-                    != 0,
-            )),
+            y_parity: Some(Parity::from(signature.v().y_parity())),
         }
     }
 }
