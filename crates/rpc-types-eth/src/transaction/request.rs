c9980cc36e06afa3abebc6806e9417d1f1d3e805
Matthias Seitz <matthias.seitz@outlook.de>
2024-09-10 02:54:00 +0200

    chore: require destination for 7702 (#1262)
    * chore: require destination for 7702
    
    * Update crates/consensus/src/transaction/eip7702.rs
    
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>
    
    * review
    
    * clippy
    
    ---------
    
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/rpc-types-eth/src/transaction/request.rs
+++ crates/rpc-types-eth/src/transaction/request.rs
@@ -312,7 +312,7 @@ impl TransactionRequest {
     /// If required fields are missing. Use `complete_7702` to check if the
     /// request can be built.
     fn build_7702(self) -> Result<TxEip7702, &'static str> {
-        let checked_to = self.to.ok_or("Missing 'to' field for Eip7702 transaction.")?;
+        let to_address = self.to.ok_or("Missing 'to' field for Eip7702 transaction.")?.to().copied().ok_or("The field `to` can only be of type TxKind::Call(Account). Please change it accordingly.")?;
 
         Ok(TxEip7702 {
             chain_id: self.chain_id.unwrap_or(1),
@@ -324,7 +324,7 @@ impl TransactionRequest {
             max_priority_fee_per_gas: self
                 .max_priority_fee_per_gas
                 .ok_or("Missing 'max_priority_fee_per_gas' field for Eip7702 transaction.")?,
-            to: checked_to,
+            to: to_address,
             value: self.value.unwrap_or_default(),
             input: self.input.into_input().unwrap_or_default(),
             access_list: self.access_list.unwrap_or_default(),
@@ -808,7 +808,7 @@ impl From<TxEip4844Variant> for TransactionRequest {
 impl From<TxEip7702> for TransactionRequest {
     fn from(tx: TxEip7702) -> Self {
         Self {
-            to: if let TxKind::Call(to) = tx.to { Some(to.into()) } else { None },
+            to: Some(tx.to.into()),
             gas: Some(tx.gas_limit),
             max_fee_per_gas: Some(tx.max_fee_per_gas),
             max_priority_fee_per_gas: Some(tx.max_priority_fee_per_gas),
