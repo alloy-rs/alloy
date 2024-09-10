6c46c61c3082d2d301ffaa8013dc31681afe829f
refcell <abigger87@gmail.com>
2024-09-10 09:28:10 -0400

    chore: remove unused hash_map re-export

--- crates/rpc-types-eth/src/lib.rs
+++ crates/rpc-types-eth/src/lib.rs
@@ -13,10 +13,10 @@ extern crate alloc;
 pub mod collections {
     cfg_if::cfg_if! {
         if #[cfg(feature = "std")] {
-            pub use std::collections::{hash_map, hash_set, HashMap, HashSet};
+            pub use std::collections::{hash_set, HashMap, HashSet};
             use hashbrown as _;
         } else {
-            pub use hashbrown::{hash_map, hash_set, HashMap, HashSet};
+            pub use hashbrown::{hash_set, HashMap, HashSet};
         }
     }
 }
