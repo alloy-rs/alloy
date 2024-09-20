fe31e10fbc11a8d8bc76036ca004ccdaad90a104
refcell <abigger87@gmail.com>
2024-09-20 14:40:14 -0400

    feat: consensus block type

---
+++ crates/consensus/src/block.rs
@@ -0,0 +1,23 @@
+//! Genesic Block Type
+
+use crate::Header;
+use alloy_eips::eip4895::Withdrawal;
+use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
+
+/// Ethereum full block.
+///
+/// Withdrawals can be optionally included at the end of the RLP encoded message.
+///
+/// Taken from [reth-primitives](https://github.com/paradigmxyz/reth)
+#[derive(Debug, Clone, PartialEq, Eq, Default, RlpEncodable, RlpDecodable)]
+#[rlp(trailing)]
+pub struct Block<T: Encodable + Decodable> {
+    /// Block header.
+    pub header: Header,
+    /// Transactions in this block.
+    pub body: Vec<T>,
+    /// Ommers/uncles header.
+    pub ommers: Vec<Header>,
+    /// Block withdrawals.
+    pub withdrawals: Option<Vec<Withdrawal>>,
+}
