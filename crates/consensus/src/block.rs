76ff168dd347f5ffa84f82621460925dd4b97802
refcell <abigger87@gmail.com>
2024-09-23 16:32:11 -0400

    fix: remove 2718 encodables, docs

--- crates/consensus/src/block.rs
+++ crates/consensus/src/block.rs
@@ -2,10 +2,7 @@
 
 use crate::{Header, Requests};
 use alloc::vec::Vec;
-use alloy_eips::{
-    eip2718::{Decodable2718, Encodable2718},
-    eip4895::Withdrawal,
-};
+use alloy_eips::eip4895::Withdrawal;
 use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
 
 /// Ethereum full block.
@@ -13,9 +10,11 @@ use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
 /// Withdrawals can be optionally included at the end of the RLP encoded message.
 ///
 /// Taken from [reth-primitives](https://github.com/paradigmxyz/reth)
+///
+/// See p2p block encoding reference: <https://github.com/ethereum/devp2p/blob/master/caps/eth.md#block-encoding-and-validity>
 #[derive(Debug, Clone, PartialEq, Eq, Default, RlpEncodable, RlpDecodable)]
 #[rlp(trailing)]
-pub struct Block<T: Encodable + Decodable + Encodable2718 + Decodable2718> {
+pub struct Block<T: Encodable + Decodable> {
     /// Block header.
     pub header: Header,
     /// Block body.
@@ -23,10 +22,12 @@ pub struct Block<T: Encodable + Decodable + Encodable2718 + Decodable2718> {
     pub body: BlockBody<T>,
 }
 
-/// A block body.
+/// A response to `GetBlockBodies`, containing bodies if any bodies were found.
+///
+/// Withdrawals can be optionally included at the end of the RLP encoded message.
 #[derive(Debug, Clone, PartialEq, Eq, Default, RlpEncodable, RlpDecodable)]
 #[rlp(trailing)]
-pub struct BlockBody<T: Encodable + Decodable + Encodable2718 + Decodable2718> {
+pub struct BlockBody<T: Encodable + Decodable> {
     /// Transactions in this block.
     pub transactions: Vec<T>,
     /// Ommers/uncles header.
