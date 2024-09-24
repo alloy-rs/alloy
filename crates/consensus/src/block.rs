f956528823301a789919bc18b1ac5892a43c9ccb
Arsenii Kulikov <klkvrr@gmail.com>
2024-09-24 14:25:52 +0300

    manual rlp

--- crates/consensus/src/block.rs
+++ crates/consensus/src/block.rs
@@ -12,13 +12,11 @@ use alloy_rlp::{Decodable, Encodable, RlpDecodable, RlpEncodable};
 /// Taken from [reth-primitives](https://github.com/paradigmxyz/reth)
 ///
 /// See p2p block encoding reference: <https://github.com/ethereum/devp2p/blob/master/caps/eth.md#block-encoding-and-validity>
-#[derive(Debug, Clone, PartialEq, Eq, Default, RlpEncodable, RlpDecodable)]
-#[rlp(trailing)]
-pub struct Block<T: Encodable + Decodable> {
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+pub struct Block<T> {
     /// Block header.
     pub header: Header,
     /// Block body.
-    #[rlp(flatten)]
     pub body: BlockBody<T>,
 }
 
@@ -27,7 +25,7 @@ pub struct Block<T: Encodable + Decodable> {
 /// Withdrawals can be optionally included at the end of the RLP encoded message.
 #[derive(Debug, Clone, PartialEq, Eq, Default, RlpEncodable, RlpDecodable)]
 #[rlp(trailing)]
-pub struct BlockBody<T: Encodable + Decodable> {
+pub struct BlockBody<T> {
     /// Transactions in this block.
     pub transactions: Vec<T>,
     /// Ommers/uncles header.
@@ -36,4 +34,63 @@ pub struct BlockBody<T: Encodable + Decodable> {
     pub withdrawals: Option<Vec<Withdrawal>>,
     /// Block requests
     pub requests: Option<Requests>,
+}
+
+/// We need to implement RLP traits manually because we currently don't have a way to flatten
+/// [`BlockBody`] into [`Block`].
+mod block_rlp {
+    use super::*;
+
+    #[derive(RlpDecodable)]
+    #[rlp(trailing)]
+    struct Helper<T> {
+        header: Header,
+        transactions: Vec<T>,
+        ommers: Vec<Header>,
+        withdrawals: Option<Vec<Withdrawal>>,
+        requests: Option<Requests>,
+    }
+
+    #[derive(RlpEncodable)]
+    #[rlp(trailing)]
+    struct HelperRef<'a, T> {
+        header: &'a Header,
+        transactions: &'a Vec<T>,
+        ommers: &'a Vec<Header>,
+        withdrawals: Option<&'a Vec<Withdrawal>>,
+        requests: Option<&'a Requests>,
+    }
+
+    impl<'a, T> From<&'a Block<T>> for HelperRef<'a, T> {
+        fn from(block: &'a Block<T>) -> Self {
+            let Block { header, body: BlockBody { transactions, ommers, withdrawals, requests } } =
+                block;
+            Self {
+                header,
+                transactions,
+                ommers,
+                withdrawals: withdrawals.as_ref(),
+                requests: requests.as_ref(),
+            }
+        }
+    }
+
+    impl<T: Encodable> Encodable for Block<T> {
+        fn length(&self) -> usize {
+            let helper: HelperRef<'_, T> = self.into();
+            helper.length()
+        }
+
+        fn encode(&self, out: &mut dyn alloy_rlp::bytes::BufMut) {
+            let helper: HelperRef<'_, T> = self.into();
+            helper.encode(out)
+        }
+    }
+
+    impl<T: Decodable> Decodable for Block<T> {
+        fn decode(b: &mut &[u8]) -> alloy_rlp::Result<Self> {
+            let Helper { header, transactions, ommers, withdrawals, requests } = Helper::decode(b)?;
+            Ok(Self { header, body: BlockBody { transactions, ommers, withdrawals, requests } })
+        }
+    }
 }
