84956fed176693143acc08b5b295500adbb3eac3
Alexey Shekhirin <a.shekhirin@gmail.com>
2024-09-29 14:41:25 +0300

    more info about limitation

--- crates/consensus/src/lib.rs
+++ crates/consensus/src/lib.rs
@@ -54,6 +54,12 @@ mod signed;
 pub use signed::Signed;
 
 /// Bincode-compatible serde implementations for consensus types.
+///
+/// `bincode` crate doesn't work well with optionally serializable serde fields, but some of the
+/// consensus types require optional serialization for RPC compatibility. This module makes so that
+/// all fields are serialized.
+///
+/// Read more: <https://github.com/bincode-org/bincode/issues/326>
 #[cfg(all(feature = "serde", feature = "serde-bincode-compat"))]
 pub mod serde_bincode_compat {
     pub use super::{
