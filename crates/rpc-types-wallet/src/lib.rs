b93ff616bc643970eaf4cc3cbc95ad6d1fe4e721
nadtech-hub <taberlick@gmail.com>
2024-10-11 13:08:01 +0200

    atrr 'check unused deps removed'
    Signed-off-by: nadtech-hub <taberlick@gmail.com>

--- crates/rpc-types-wallet/src/lib.rs
+++ crates/rpc-types-wallet/src/lib.rs
@@ -4,7 +4,6 @@
     html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-#![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
 mod wallet;
 pub use wallet::*;
