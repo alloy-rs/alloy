3ca2358e4dccdc7fdbab7003f4ca7f5562a493cd
Thomas Coratger <thomas.coratger@gmail.com>
2024-10-15 16:23:00 +0200

    fix comments

--- crates/consensus/src/transaction/mod.rs
+++ crates/consensus/src/transaction/mod.rs
@@ -3,7 +3,7 @@
 use crate::Signed;
 use alloc::vec::Vec;
 use alloy_eips::{eip2930::AccessList, eip7702::SignedAuthorization};
-use alloy_primitives::{keccak256, ChainId, TxKind, B256, U256};
+use alloy_primitives::{keccak256, Address, ChainId, TxKind, B256, U256};
 use core::any;
 
 mod eip1559;
@@ -111,6 +111,14 @@ pub trait Transaction: any::Any + Send + Sync + 'static {
 
     /// Returns the transaction kind.
     fn kind(&self) -> TxKind;
+
+    /// Get the transaction's address of the contract that will be called, or the address that will
+    /// receive the transfer.
+    ///
+    /// Returns `None` if this is a `CREATE` transaction.
+    fn to(&self) -> Option<Address> {
+        self.kind().to().copied()
+    }
 
     /// Get `value`.
     fn value(&self) -> U256;
