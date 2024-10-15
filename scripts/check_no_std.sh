6b936f398a02ff0cb2368c992649758a04bbb34e
Jonathan Brown <jbrown@acuity.network>
2024-10-15 18:48:48 +0700

    fix(wasm): move WebSocket build check from script to GitHub CI

--- scripts/check_no_std.sh
+++ scripts/check_no_std.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-target=wasm32-unknown-unknown
+target=riscv32imac-unknown-none-elf
 crates=(
     alloy-eips
     alloy-genesis
     alloy-serde
     alloy-consensus
     alloy-network-primitives
-    alloy-rpc-client
     alloy-rpc-types-eth
     alloy-rpc-types-engine
-    alloy-transport
-    alloy-transport-ws
 )
 
-cmd=(cargo +stable hack check --no-default-features --ignore-unknown-features --features ws --target "$target")
+cmd=(cargo +stable hack check --no-default-features --target "$target")
 for crate in "${crates[@]}"; do
     cmd+=(-p "$crate")
 done
