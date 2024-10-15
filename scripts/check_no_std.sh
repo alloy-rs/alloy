1888ea55f40007f1dee6073c04c18fc47a2ac218
Jonathan Brown <jbrown@acuity.network>
2024-10-15 15:09:22 +0700

    fix(wasm): support ws

--- scripts/check_no_std.sh
+++ scripts/check_no_std.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-target=riscv32imac-unknown-none-elf
+target=wasm32-unknown-unknown
 crates=(
     alloy-eips
     alloy-genesis
     alloy-serde
     alloy-consensus
     alloy-network-primitives
+    alloy-rpc-client
     alloy-rpc-types-eth
     alloy-rpc-types-engine
+    alloy-transport
+    alloy-transport-ws
 )
 
-cmd=(cargo +stable hack check --no-default-features --target "$target")
+cmd=(cargo +stable hack check --no-default-features --ignore-unknown-features --features ws --target "$target")
 for crate in "${crates[@]}"; do
     cmd+=(-p "$crate")
 done
