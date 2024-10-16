47f0d069e795080d67972091c1e04aee76de9054
James Prestwich <james@prestwi.ch>
2024-10-15 20:56:31 -0400

    fix: rename gas_limit to gas in serde def for txns (#1486)

--- crates/consensus/src/transaction/eip2930.rs
+++ crates/consensus/src/transaction/eip2930.rs
@@ -32,7 +32,7 @@ pub struct TxEip2930 {
     /// this transaction. This is paid up-front, before any
     /// computation is done and may not be increased
     /// later; formally Tg.
-    #[cfg_attr(feature = "serde", serde(with = "alloy_serde::quantity"))]
+    #[cfg_attr(feature = "serde", serde(with = "alloy_serde::quantity", rename = "gas"))]
     pub gas_limit: u64,
     /// The 160-bit address of the message call’s recipient or, for a contract creation
     /// transaction, ∅, used here to denote the only member of B0 ; formally Tt.
