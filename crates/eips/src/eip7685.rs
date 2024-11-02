7e555bcd09a9a58ef54761a823b4ef2aecaf41ca
tesseract <146037313+DoTheBestToGetTheBest@users.noreply.github.com>
2024-11-02 07:38:25 -0700

    Update eip7685.rs

--- crates/eips/src/eip7685.rs
+++ crates/eips/src/eip7685.rs
@@ -78,16 +78,21 @@ impl Requests {
 }
 
 /// A list of requests or a precomputed requests hash.
+///
+/// For testing purposes, the `Hash` variant stores a precomputed requests hash. This can be useful
+///   when the exact contents of the requests are unnecessary, and only a consistent hash value is
+/// needed   to simulate the presence of requests without holding actual data.
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub enum PragueRequests {
-    /// Stores a list of requests for dynamic requests hash calculation.
+pub enum RequestsOrHash {
+    /// Stores a list of requests, allowing for dynamic requests hash calculation.
     Requests(Requests),
-    /// Stores a precomputed requests hash.
+    /// Stores a precomputed requests hash, used primarily for testing.
     Hash(B256),
 }
 
-impl PragueRequests {
+impl RequestsOrHash {
     /// Returns the requests hash for the enum instance.
     ///
     /// - If the instance contains a list of requests, this function calculates the hash using
