889c36657471c36159dec4212327468bb29ebf94
Daniel Beal <git@dbeal.dev>
2024-11-17 19:17:33 +0900

    fix(consensus): transaction types should be able to deserialize legacy gas_limit name
    the `gas_limit` field in the transaction types was renamed to serialize to `gas` instead of `gasLimit`
    
    However, this causes old state dumps prior to this change to fail to load (in fact, downstream `foundry` tests were modified in order to accomodate for this, although I really don't think this should have happened) https://github.com/foundry-rs/foundry/commit/7b118faeff4848be480f9c30c11237b9e9e6eb31 .

--- crates/consensus/src/transaction/eip7702.rs
+++ crates/consensus/src/transaction/eip7702.rs
@@ -30,7 +30,7 @@ pub struct TxEip7702 {
     /// this transaction. This is paid up-front, before any
     /// computation is done and may not be increased
     /// later; formally Tg.
-    #[cfg_attr(feature = "serde", serde(with = "alloy_serde::quantity", rename = "gas"))]
+    #[cfg_attr(feature = "serde", serde(with = "alloy_serde::quantity", alias = "gasLimit", rename = "gas"))]
     pub gas_limit: u64,
     /// A scalar value equal to the maximum
     /// amount of gas that should be used in executing
