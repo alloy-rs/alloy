a59d90e6c7ef782ec92c820f921b7567a204847a
Arsenii Kulikov <klkvrr@gmail.com>
2024-12-01 01:09:23 +0400

    wip

--- crates/rpc-types-any/src/transaction/receipt.rs
+++ crates/rpc-types-any/src/transaction/receipt.rs
@@ -1,12 +1,10 @@
-use alloy_consensus::Receipt;
 use alloy_consensus_any::AnyReceiptEnvelope;
 use alloy_rpc_types_eth::{Log, TransactionReceipt};
 use alloy_serde::WithOtherFields;
 
 /// Alias for a catch-all receipt type.
 #[doc(alias = "AnyTxReceipt")]
-pub type AnyTransactionReceipt =
-    WithOtherFields<TransactionReceipt<AnyReceiptEnvelope<Receipt<Log>>>>;
+pub type AnyTransactionReceipt = WithOtherFields<TransactionReceipt<AnyReceiptEnvelope<Log>>>;
 
 #[cfg(test)]
 mod test {
