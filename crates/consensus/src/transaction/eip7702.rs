7b7a472ccbd40fd1f54b313c6af1cd1c4b8dd130
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-09 15:06:08 +0100

    Revert "Revert "feat(consensus): require typed2718 for transaction (#1746)" (…"
    This reverts commit 40c5fa7d79d122c4fab0b5e8d419b1624c0ddd69.

--- crates/consensus/src/transaction/eip7702.rs
+++ crates/consensus/src/transaction/eip7702.rs
@@ -1,4 +1,4 @@
-use crate::{SignableTransaction, Signed, Transaction, TxType};
+use crate::{SignableTransaction, Signed, Transaction, TxType, Typed2718};
 use alloc::vec::Vec;
 use alloy_eips::{
     eip2930::AccessList,
@@ -233,11 +233,6 @@ impl Transaction for TxEip7702 {
     }
 
     #[inline]
-    fn ty(&self) -> u8 {
-        TxType::Eip7702 as u8
-    }
-
-    #[inline]
     fn access_list(&self) -> Option<&AccessList> {
         Some(&self.access_list)
     }
@@ -271,6 +266,12 @@ impl SignableTransaction<Signature> for TxEip7702 {
         let tx_hash = self.tx_hash(&signature);
 
         Signed::new_unchecked(self, signature, tx_hash)
+    }
+}
+
+impl Typed2718 for TxEip7702 {
+    fn ty(&self) -> u8 {
+        TxType::Eip7702 as u8
     }
 }
 
