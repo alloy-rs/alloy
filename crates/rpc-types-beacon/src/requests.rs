3b959d9dcf12799d63fb66a3746e0a9cbd794c6a
Ryan Schneider <ryanleeschneider@gmail.com>
2024-12-11 14:19:05 -0800

    feat(relay): Re-add {Deposit,Withdrawal,Consolidation}Request objects for relay submissions

---
+++ crates/rpc-types-beacon/src/requests.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+use alloy_eips::eip6110::DepositRequest;
+use alloy_eips::eip7002::WithdrawalRequest;
+use alloy_eips::eip7251::ConsolidationRequest;
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(deny_unknown_fields)]
+#[serde(rename_all = "snake_case")]
+#[cfg_attr(feature = "ssz", derive(ssz_derive::Decode, ssz_derive::Encode))]
+/// An Electra-compatible execution requests payload.
+pub struct ExecutionRequestsV4 {
+    /// The requested deposits.
+    pub deposits: Vec<DepositRequest>,
+    /// The requested withdrawals.
+    pub withdrawals: Vec<WithdrawalRequest>,
+    /// The requested consolidations.
+    pub consolidations: Vec<ConsolidationRequest>,
+}
