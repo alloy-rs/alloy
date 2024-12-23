22d704d3e424f18436168070a8476efca15ab1f0
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-23 11:54:07 +0100

    chore: replace derive_more with thiserror (#1822)

--- crates/rpc-types-eth/src/transaction/error.rs
+++ crates/rpc-types-eth/src/transaction/error.rs
@@ -2,16 +2,9 @@ use alloc::string::String;
 
 /// Error variants when converting from [crate::Transaction] to [alloy_consensus::Signed]
 /// transaction.
-#[derive(Debug, derive_more::Display)]
+#[derive(Debug, thiserror::Error)]
 pub enum ConversionError {
     /// A custom Conversion Error that doesn't fit other categories.
-    #[display("conversion error: {_0}")]
+    #[error("conversion error: {0}")]
     Custom(String),
-}
-
-#[cfg(feature = "std")]
-impl std::error::Error for ConversionError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        None
-    }
 }
--- crates/rpc-types-eth/src/transaction/request.rs
+++ crates/rpc-types-eth/src/transaction/request.rs
@@ -1099,14 +1099,11 @@ impl From<Option<Bytes>> for TransactionInput {
 }
 
 /// Error thrown when both `data` and `input` fields are set and not equal.
-#[derive(Debug, Default, derive_more::Display)]
-#[display("both \"data\" and \"input\" are set and not equal. Please use \"input\" to pass transaction call data")]
+#[derive(Debug, Default, thiserror::Error)]
+#[error("both \"data\" and \"input\" are set and not equal. Please use \"input\" to pass transaction call data")]
 #[doc(alias = "TxInputError")]
 #[non_exhaustive]
 pub struct TransactionInputError;
-
-#[cfg(feature = "std")]
-impl std::error::Error for TransactionInputError {}
 
 /// Error thrown when a transaction request cannot be built into a transaction.
 #[derive(Debug)]
