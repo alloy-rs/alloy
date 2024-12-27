5a5b66119d236e8ac781bb53603e40ce97ffb0b6
Matthias Seitz <matthias.seitz@outlook.de>
2024-12-27 15:35:16 +0100

    feat: add tryfrom payloadv3 for block

--- crates/rpc-types-engine/src/payload.rs
+++ crates/rpc-types-engine/src/payload.rs
@@ -445,6 +445,28 @@ impl ExecutionPayloadV3 {
     pub const fn timestamp(&self) -> u64 {
         self.payload_inner.payload_inner.timestamp
     }
+
+    /// Converts [`ExecutionPayloadV3`] to [`Block`]
+    // This performs the same conversion as the underlying V2 payload, but inserts the blob gas used
+    // and excess blob gas.
+    ///
+    /// See also [`ExecutionPayloadV2::try_into_block`].
+    pub fn try_into_block<T: Decodable2718>(self) -> Result<Block<T>, PayloadError> {
+        let mut base_block = self.payload_inner.try_into_block()?;
+
+        base_block.header.blob_gas_used = Some(self.blob_gas_used);
+        base_block.header.excess_blob_gas = Some(self.excess_blob_gas);
+
+        Ok(base_block)
+    }
+}
+
+impl<T: Decodable2718> TryFrom<ExecutionPayloadV3> for Block<T> {
+    type Error = PayloadError;
+
+    fn try_from(value: ExecutionPayloadV3) -> Result<Self, Self::Error> {
+        value.try_into_block()
+    }
 }
 
 #[cfg(feature = "ssz")]
