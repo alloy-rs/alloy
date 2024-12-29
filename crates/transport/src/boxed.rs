c4a1ff475b3edd27dcd27f68fab46f62646d5c7b
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2024-12-29 18:32:47 +0100

    docs

--- crates/transport/src/boxed.rs
+++ crates/transport/src/boxed.rs
@@ -3,6 +3,27 @@ use alloy_json_rpc::{RequestPacket, ResponsePacket};
 use std::fmt;
 use tower::Service;
 
+#[allow(unnameable_types)]
+mod private {
+    pub trait Sealed {}
+    impl<T: super::Transport + Clone> Sealed for T {}
+}
+
+/// Trait for converting a transport into a boxed transport.
+///
+/// This trait is sealed and implemented for all types that implement
+/// [`Transport`] + [`Clone`].
+pub trait IntoBoxTransport: Transport + Clone + private::Sealed {
+    /// Boxes the transport.
+    fn into_box_transport(self) -> BoxTransport;
+}
+
+impl<T: Transport + Clone> IntoBoxTransport for T {
+    fn into_box_transport(self) -> BoxTransport {
+        BoxTransport { inner: Box::new(self) }
+    }
+}
+
 /// A boxed, Clone-able [`Transport`] trait object.
 ///
 /// This type allows RPC clients to use a type-erased transport. It is
@@ -10,23 +31,23 @@ use tower::Service;
 /// allows for complex behavior abstracting across several different clients
 /// with different transport types.
 ///
-/// Most higher-level types will be generic over `T: Transport = BoxTransport`.
-/// This allows parameterization with a concrete type, while hiding this
-/// complexity from the library consumer.
+/// All higher-level types, such as [`RpcClient`], use this type internally
+/// rather than a generic [`Transport`] parameter.
 ///
 /// [`RpcClient`]: crate::client::RpcClient
-#[repr(transparent)]
 pub struct BoxTransport {
-    inner: Box<dyn CloneTransport + Send + Sync>,
+    inner: Box<dyn CloneTransport>,
 }
 
 impl BoxTransport {
     /// Instantiate a new box transport from a suitable transport.
+    #[inline]
     pub fn new<T: IntoBoxTransport>(transport: T) -> Self {
         transport.into_box_transport()
     }
 
     /// Returns a reference to the inner transport.
+    #[inline]
     pub fn as_any(&self) -> &dyn std::any::Any {
         self.inner.as_any()
     }
@@ -44,24 +65,6 @@ impl Clone for BoxTransport {
     }
 }
 
-#[allow(unnameable_types)]
-mod private {
-    pub trait Sealed {}
-    impl<T: super::Transport + Clone> Sealed for T {}
-}
-
-/// Trait for converting a transport into a boxed transport.
-pub trait IntoBoxTransport: Transport + Clone + private::Sealed {
-    /// Boxes the transport.
-    fn into_box_transport(self) -> BoxTransport;
-}
-
-impl<T: Transport + Clone> IntoBoxTransport for T {
-    fn into_box_transport(self) -> BoxTransport {
-        BoxTransport { inner: Box::new(self) }
-    }
-}
-
 /// Helper trait for constructing [`BoxTransport`].
 trait CloneTransport: Transport + std::any::Any {
     fn clone_box(&self) -> Box<dyn CloneTransport + Send + Sync>;
@@ -72,10 +75,12 @@ impl<T> CloneTransport for T
 where
     T: Transport + Clone + Send + Sync,
 {
+    #[inline]
     fn clone_box(&self) -> Box<dyn CloneTransport + Send + Sync> {
         Box::new(self.clone())
     }
 
+    #[inline]
     fn as_any(&self) -> &dyn std::any::Any {
         self
     }
@@ -88,6 +93,7 @@ impl Service<RequestPacket> for BoxTransport {
 
     type Future = TransportFut<'static>;
 
+    #[inline]
     fn poll_ready(
         &mut self,
         cx: &mut std::task::Context<'_>,
@@ -95,6 +101,7 @@ impl Service<RequestPacket> for BoxTransport {
         self.inner.poll_ready(cx)
     }
 
+    #[inline]
     fn call(&mut self, req: RequestPacket) -> Self::Future {
         self.inner.call(req)
     }
@@ -105,10 +112,12 @@ mod test {
     use super::*;
 
     // checks trait + send + sync + 'static
-    fn __compile_check() {
-        fn inner<T: CloneTransport>(_: Option<T>) {}
-        fn inner_2<T: Transport>(_: Option<T>) {}
-        inner::<BoxTransport>(None);
-        inner::<BoxTransport>(None);
+    fn _compile_check() {
+        fn inner<T>()
+        where
+            T: Transport + CloneTransport + Send + Sync + Clone + IntoBoxTransport + 'static,
+        {
+        }
+        inner::<BoxTransport>();
     }
 }
