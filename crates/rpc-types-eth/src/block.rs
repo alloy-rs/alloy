b2b0c50be86bdc92d0af48c4f71fcf62cfe8557e
Matthias Seitz <matthias.seitz@outlook.de>
2025-01-27 23:47:10 +0100

    feat: add into sealed for rpc header

--- crates/rpc-types-eth/src/block.rs
+++ crates/rpc-types-eth/src/block.rs
@@ -258,6 +258,11 @@ impl<H> Header<H> {
         Self { hash, inner, total_difficulty: None, size: None }
     }
 
+    /// Consumes the type and returns the [`Sealed`] header.
+    pub fn into_sealed(self) -> Sealed<H> {
+        Sealed::new_unchecked(self.inner, self.hash)
+    }
+
     /// Consumes the type and returns the wrapped consensus header.
     pub fn into_consensus(self) -> H {
         self.inner
@@ -430,6 +435,12 @@ impl From<Header> for alloy_consensus::Header {
     }
 }
 
+impl<H> From<Header<H>> for Sealed<H> {
+    fn from(value: Header<H>) -> Self {
+        value.into_sealed()
+    }
+}
+
 /// Error that can occur when converting other types to blocks
 #[derive(Clone, Copy, Debug, thiserror::Error)]
 pub enum BlockError {
@@ -551,12 +562,11 @@ pub struct BadBlock {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use alloy_primitives::{hex, keccak256, Bloom, B64};
     use arbitrary::Arbitrary;
     use rand::Rng;
     use similar_asserts::assert_eq;
-
-    use super::*;
 
     #[test]
     fn arbitrary_header() {
