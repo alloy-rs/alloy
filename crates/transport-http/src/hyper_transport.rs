be494ed0128ea06cb8bbe7b75dec0a4301fccc5f
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-01-30 21:49:02 +0100

    feat: use StatusCode::is_success instead of just OK (#1974)
    We currently only accept status code 200, whereas any code in 200..300
    is a valid success code.

--- crates/transport-http/src/hyper_transport.rs
+++ crates/transport-http/src/hyper_transport.rs
@@ -118,7 +118,7 @@ where
         debug!(bytes = body.len(), "retrieved response body. Use `trace` for full body");
         trace!(body = %String::from_utf8_lossy(&body), "response body");
 
-        if status != hyper::StatusCode::OK {
+        if !status.is_success() {
             return Err(TransportErrorKind::http_error(
                 status.as_u16(),
                 String::from_utf8_lossy(&body).into_owned(),
