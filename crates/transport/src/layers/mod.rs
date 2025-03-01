a62c8ea85e7748b32f0ddfdc260e8723cf3e11d7
nicolas <48695862+merklefruit@users.noreply.github.com>
2025-03-01 12:55:46 +0100

    feat: initial impl of fallback layer and service

--- crates/transport/src/layers/mod.rs
+++ crates/transport/src/layers/mod.rs
@@ -4,3 +4,8 @@ mod retry;
 
 /// RetryBackoffLayer
 pub use retry::{RateLimitRetryPolicy, RetryBackoffLayer, RetryBackoffService, RetryPolicy};
+
+mod fallback;
+
+/// FallbackLayer
+pub use fallback::{FallbackLayer, FallbackService};
