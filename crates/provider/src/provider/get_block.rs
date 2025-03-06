43c2edfad8bddde2a44cb9d5d02c6e64e8137863
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-03-06 19:31:51 +0530

    fix(`provider`): custom deser for pending blocks (#2146)
    * fix(`rpc-types`): account for missing/null fields in `Header`
    
    * clippy
    
    * no custom deser for `Header`
    
    * custom deser for pending block requests
    
    * docs
    
    * fix

--- crates/provider/src/provider/get_block.rs
+++ crates/provider/src/provider/get_block.rs
@@ -5,9 +5,10 @@ use alloy_eips::{BlockId, BlockNumberOrTag};
 use alloy_json_rpc::RpcRecv;
 use alloy_network::BlockResponse;
 use alloy_network_primitives::BlockTransactionsKind;
-use alloy_primitives::BlockHash;
+use alloy_primitives::{Address, BlockHash, B256, B64};
 use alloy_rpc_client::{ClientRef, RpcCall};
-use alloy_transport::TransportResult;
+use alloy_transport::{TransportError, TransportResult};
+use serde_json::Value;
 
 /// The parameters for an `eth_getBlockBy{Hash, Number}` RPC request.
 ///
@@ -70,9 +71,7 @@ where
     /// `"eth_getBlockByHash"`.
     pub fn by_hash(hash: BlockHash, client: ClientRef<'_>) -> Self {
         let params = EthGetBlockParams::default();
-        let call = client
-            .request("eth_getBlockByHash", params)
-            .map_resp(utils::convert_to_hashes as fn(Option<BlockResp>) -> Option<BlockResp>);
+        let call = client.request("eth_getBlockByHash", params);
         Self::new_rpc(hash.into(), call)
     }
 
@@ -80,10 +79,12 @@ where
     /// `"eth_getBlockByNumber"`.
     pub fn by_number(number: BlockNumberOrTag, client: ClientRef<'_>) -> Self {
         let params = EthGetBlockParams::default();
-        let call = client
-            .request("eth_getBlockByNumber", params)
-            .map_resp(utils::convert_to_hashes as fn(Option<BlockResp>) -> Option<BlockResp>);
-        Self::new_rpc(number.into(), call)
+
+        if number.is_pending() {
+            return Self::new_pending_rpc(client.request("eth_getBlockByNumber", params));
+        }
+
+        Self::new_rpc(number.into(), client.request("eth_getBlockByNumber", params))
     }
 }
 
@@ -96,6 +97,16 @@ where
         Self {
             block,
             inner: GetBlockInner::RpcCall(inner),
+            kind: BlockTransactionsKind::Hashes,
+            _pd: PhantomData,
+        }
+    }
+
+    /// Create a new [`EthGetBlock`] request with the given [`RpcCall`] for pending block.
+    pub fn new_pending_rpc(inner: RpcCall<EthGetBlockParams, Value>) -> Self {
+        Self {
+            block: BlockId::pending(),
+            inner: GetBlockInner::PendingBlock(inner),
             kind: BlockTransactionsKind::Hashes,
             _pd: PhantomData,
         }
@@ -143,7 +154,57 @@ where
             GetBlockInner::RpcCall(call) => {
                 let rpc_call =
                     call.map_params(|_params| EthGetBlockParams::new(self.block, self.kind));
-                ProviderCall::RpcCall(rpc_call)
+
+                let fut = async move {
+                    let resp = rpc_call.await?;
+                    let result =
+                        if self.kind.is_hashes() { utils::convert_to_hashes(resp) } else { resp };
+                    Ok(result)
+                };
+
+                ProviderCall::BoxedFuture(Box::pin(fut))
+            }
+            GetBlockInner::PendingBlock(call) => {
+                let rpc_call =
+                    call.map_params(|_params| EthGetBlockParams::new(self.block, self.kind));
+
+                let map_fut = async move {
+                    let mut block = rpc_call.await?;
+
+                    if block.is_null() {
+                        return Ok(None);
+                    }
+
+                    // Ref: <https://github.com/alloy-rs/alloy/issues/2117>
+                    // Geth ref: <https://github.com/ethereum/go-ethereum/blob/ebff2f42c0fbb4ebee43b0e73e39b658305a8a9b/internal/ethapi/api.go#L470-L471>
+                    tracing::trace!(pending_block = ?block.to_string());
+                    if block.get("hash").map_or(true, |v| v.is_null()) {
+                        block["hash"] = Value::String(format!("{}", B256::ZERO));
+                    }
+
+                    if block.get("nonce").map_or(true, |v| v.is_null()) {
+                        block["nonce"] = Value::String(format!("{}", B64::ZERO));
+                    }
+
+                    if block.get("miner").map_or(true, |v| v.is_null())
+                        || block.get("beneficiary").map_or(true, |v| v.is_null())
+                    {
+                        block["miner"] = Value::String(format!("{}", Address::ZERO));
+                    }
+
+                    let block = serde_json::from_value(block.clone())
+                        .map_err(|e| TransportError::deser_err(e, block.to_string()))?;
+
+                    let block = if self.kind.is_hashes() {
+                        utils::convert_to_hashes(Some(block))
+                    } else {
+                        Some(block)
+                    };
+
+                    Ok(block)
+                };
+
+                ProviderCall::BoxedFuture(Box::pin(map_fut))
             }
             GetBlockInner::ProviderCall(producer) => producer(self.kind),
         }
@@ -168,6 +229,16 @@ where
 {
     /// [`RpcCall`] with params that get wrapped into [`EthGetBlockParams`] in the future.
     RpcCall(RpcCall<EthGetBlockParams, Option<BlockResp>>),
+    /// Pending Block Call
+    ///
+    /// This has been made explicit to handle cases where fields such as `hash`, `nonce`, `miner`
+    /// are either missing or set to null causing deserilization issues. See: <https://github.com/alloy-rs/alloy/issues/2117>
+    ///
+    /// This is specifically true in case of the response is returned from a geth node. See: <https://github.com/ethereum/go-ethereum/blob/ebff2f42c0fbb4ebee43b0e73e39b658305a8a9b/internal/ethapi/api.go#L470-L471>
+    ///
+    /// In such case, we first deserialize to [`Value`] and then check if the fields are missing or
+    /// set to null. If so, we set them to default values.  
+    PendingBlock(RpcCall<EthGetBlockParams, Value>),
     /// Closure that produces a [`ProviderCall`] given [`BlockTransactionsKind`].
     ProviderCall(ProviderCallProducer<BlockResp>),
 }
@@ -179,7 +250,23 @@ where
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::RpcCall(call) => f.debug_tuple("RpcCall").field(call).finish(),
+            Self::PendingBlock(call) => f.debug_tuple("PendingBlockCall").field(call).finish(),
             Self::ProviderCall(_) => f.debug_struct("ProviderCall").finish(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{Provider, ProviderBuilder};
+
+    // <https://github.com/alloy-rs/alloy/issues/2117>
+    #[tokio::test]
+    async fn test_pending_block_deser() {
+        let provider =
+            ProviderBuilder::new().on_http("https://binance.llamarpc.com".parse().unwrap());
+
+        let _block = provider.get_block_by_number(BlockNumberOrTag::Pending).await.unwrap();
     }
 }
