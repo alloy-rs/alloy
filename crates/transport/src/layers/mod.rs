564ec1324969747d72a085c7ac759f239632e00f
0xKitsune <0xkitsune@protonmail.com>
2025-03-06 23:34:52 -0500

    add throttle layer to transport layers

--- crates/transport/src/layers/mod.rs
+++ crates/transport/src/layers/mod.rs
@@ -1,6 +1,10 @@
 //! Module for housing transport layers.
 
 mod retry;
+mod throttle;
 
 /// RetryBackoffLayer
 pub use retry::{RateLimitRetryPolicy, RetryBackoffLayer, RetryBackoffService, RetryPolicy};
+
+/// ThrottleLayer
+pub use throttle::{ThrottleLayer, ThrottleService};
---
+++ crates/transport/src/layers/throttle.rs
@@ -0,0 +1,83 @@
+use crate::{TransportError, TransportFut};
+use alloy_json_rpc::{RequestPacket, ResponsePacket};
+use governor::{
+    clock::{QuantaClock, QuantaInstant},
+    middleware::NoOpMiddleware,
+    state::{InMemoryState, NotKeyed},
+    Quota, RateLimiter,
+};
+use std::{
+    num::NonZeroU32,
+    sync::Arc,
+    task::{Context, Poll},
+};
+use tower::{Layer, Service};
+
+/// A rate limiter for throttling RPC requests.
+type Throttle = RateLimiter<NotKeyed, InMemoryState, QuantaClock, NoOpMiddleware<QuantaInstant>>;
+
+/// A Transport Layer responsible for throttling RPC requests.
+#[derive(Debug)]
+pub struct ThrottleLayer {
+    /// Rate limiter used to throttle requests.
+    pub throttle: Arc<Throttle>,
+}
+
+impl ThrottleLayer {
+    /// Creates a new throttle layer with the specified requests per second.
+    /// # Panics
+    /// Panics if requests_per_second is not a non-zero positive integer.
+    pub fn new(requests_per_second: u32) -> Self {
+        let quota = Quota::per_second(
+            NonZeroU32::new(requests_per_second)
+                .expect("requests_per_second must be a non-zero positive integer"),
+        );
+
+        let throttle = Arc::new(RateLimiter::direct(quota));
+
+        ThrottleLayer { throttle }
+    }
+}
+
+/// A Tower Service used by the ThrottleLayer that is responsible for throttling rpc requests.
+#[derive(Debug, Clone)]
+pub struct ThrottleService<S> {
+    /// The inner service
+    inner: S,
+    throttle: Arc<Throttle>,
+}
+
+impl<S> Layer<S> for ThrottleLayer {
+    type Service = ThrottleService<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        ThrottleService { inner, throttle: self.throttle.clone() }
+    }
+}
+
+impl<S> Service<RequestPacket> for ThrottleService<S>
+where
+    S: Service<RequestPacket, Response = ResponsePacket, Error = TransportError>
+        + Send
+        + 'static
+        + Clone,
+    S::Future: Send + 'static,
+{
+    type Response = ResponsePacket;
+    type Error = TransportError;
+    type Future = TransportFut<'static>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, request: RequestPacket) -> Self::Future {
+        let throttle = self.throttle.clone();
+        let mut inner = self.inner.clone();
+
+        Box::pin(async move {
+            throttle.until_ready().await;
+            inner.call(request).await
+        })
+    }
+}
