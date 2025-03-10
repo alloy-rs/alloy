cfaf1b411ed26ff14ce9c0658ccb2f18ca84de78
Ayush Dubey <61616662+Ayushdubey86@users.noreply.github.com>
2025-03-10 22:28:25 +0530

    Update eip2930.rs

--- crates/consensus/src/transaction/eip2930.rs
+++ crates/consensus/src/transaction/eip2930.rs
@@ -83,8 +83,6 @@ impl TxEip2930 {
 }
 
 impl RlpEcdsaEncodableTx for TxEip2930 {
-    const DEFAULT_TX_TYPE: u8 = { Self::tx_type() as u8 };
-
     /// Outputs the length of the transaction's fields, without a RLP header.
     fn rlp_encoded_fields_length(&self) -> usize {
         self.chain_id.length()
