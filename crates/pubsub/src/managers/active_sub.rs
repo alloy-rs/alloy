618d1488eadf551a37924039e3cde009f90eb680
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-03-19 19:32:06 +0530

    fix(`pubsub`): fix race condition in ActiveSub

--- crates/pubsub/src/managers/active_sub.rs
+++ crates/pubsub/src/managers/active_sub.rs
@@ -2,11 +2,10 @@ use crate::RawSubscription;
 use alloy_json_rpc::SerializedRequest;
 use alloy_primitives::B256;
 use serde_json::value::RawValue;
-use std::{fmt, hash::Hash};
+use std::{fmt, hash::Hash, sync::Mutex};
 use tokio::sync::broadcast;
 
 /// An active subscription.
-#[derive(Clone)]
 pub(crate) struct ActiveSubscription {
     /// Cached hash of the request, used for sorting and equality.
     pub(crate) local_id: B256,
@@ -14,6 +13,8 @@ pub(crate) struct ActiveSubscription {
     pub(crate) request: SerializedRequest,
     /// The channel via which notifications are broadcast.
     pub(crate) tx: broadcast::Sender<Box<RawValue>>,
+    /// The channel via which notifications are received.
+    pub(crate) rx: Mutex<Option<broadcast::Receiver<Box<RawValue>>>>,
 }
 
 // NB: We implement this to prevent any incorrect future implementations.
@@ -58,8 +59,8 @@ impl ActiveSubscription {
     /// Create a new active subscription.
     pub(crate) fn new(request: SerializedRequest, channel_size: usize) -> Self {
         let local_id = request.params_hash();
-        let (tx, _rx) = broadcast::channel(channel_size);
-        Self { request, local_id, tx }
+        let (tx, rx) = broadcast::channel(channel_size);
+        Self { request, local_id, tx, rx: Mutex::new(Some(rx)) }
     }
 
     /// Serialize the request as a boxed [`RawValue`].
@@ -71,7 +72,33 @@ impl ActiveSubscription {
 
     /// Get a subscription.
     pub(crate) fn subscribe(&self) -> RawSubscription {
-        RawSubscription { rx: self.tx.subscribe(), local_id: self.local_id }
+        let new_rx = self.tx.subscribe();
+
+        // Check if any events are in queue
+        let mut pending = self.tx.len();
+        if pending == 0 {
+            return RawSubscription { rx: new_rx, local_id: self.local_id };
+        }
+
+        let lock = self.rx.lock();
+
+        if lock.is_err() {
+            return RawSubscription { rx: new_rx, local_id: self.local_id };
+        }
+        if let Some(mut rx) = lock.unwrap().take() {
+            // Attempt to drain the queue by forwarding all pending notifications to the new
+            // receiver.
+
+            while let Ok(notification) = rx.try_recv() {
+                self.notify(notification);
+                pending -= 1;
+                if pending == 0 {
+                    break;
+                }
+            }
+        }
+
+        RawSubscription { rx: new_rx, local_id: self.local_id }
     }
 
     /// Notify the subscription channel of a new value, if any receiver exists.
