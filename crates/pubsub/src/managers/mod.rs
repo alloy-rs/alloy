adb4724bfb54d58b1bb5b59320c708c3f7e0b705
Jinwei <Jinwei1987@users.noreply.github.com>
2025-04-18 20:41:12 +0800

    make PubSubFrontend new public (#2326)
    Co-authored-by: jinwei.zheng <jinwei.zheng@mirana.tech>

--- crates/pubsub/src/managers/mod.rs
+++ crates/pubsub/src/managers/mod.rs
@@ -2,7 +2,7 @@ mod active_sub;
 pub(crate) use active_sub::ActiveSubscription;
 
 mod in_flight;
-pub(crate) use in_flight::InFlight;
+pub use in_flight::InFlight;
 
 mod req;
 pub(crate) use req::RequestManager;
