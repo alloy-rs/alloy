6af4a9082af960d6bb0870ef7c4bb43708f2d3dc
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-05-01 11:42:06 +0530

    fixes

--- crates/genesis/src/lib.rs
+++ crates/genesis/src/lib.rs
@@ -11,8 +11,8 @@
 
 extern crate alloc;
 
-use alloc::{collections::BTreeMap, string::String, vec::Vec};
-use alloy_eips::{eip7840::BlobParams, eip7892::HardforkBlobParams};
+use alloc::{collections::BTreeMap, string::String};
+use alloy_eips::eip7840::BlobParams;
 use alloy_primitives::{keccak256, Address, Bytes, B256, U256};
 use alloy_serde::{storage::deserialize_storage_map, ttd::deserialize_json_ttd_opt, OtherFields};
 use alloy_trie::{TrieAccount, EMPTY_ROOT_HASH, KECCAK_EMPTY};
