51dbd9fdc6479fc2ad3a90768f24e5ed0b623138
Roman Krasiuk <rokrassyuk@gmail.com>
2025-05-12 16:48:43 +0200

    feat(eips): add more EIP-7594 constants

--- crates/eips/src/eip7594.rs
+++ crates/eips/src/eip7594.rs
@@ -1,14 +1,29 @@
-//! Constants for PeerDAS.
+//! Types and constants for PeerDAS.
 //!
 //! See also [EIP-7594](https://eips.ethereum.org/EIPS/eip-7594): PeerDAS - Peer Data Availability Sampling
-
-use crate::eip4844::FIELD_ELEMENTS_PER_BLOB;
+use crate::eip4844::{FIELD_ELEMENTS_PER_BLOB, FIELD_ELEMENT_BYTES};
+use alloy_primitives::FixedBytes;
 
 /// Number of field elements in a Reed-Solomon extended blob.
-pub const FIELD_ELEMENTS_PER_EXT_BLOB: u64 = FIELD_ELEMENTS_PER_BLOB * 2;
+pub const FIELD_ELEMENTS_PER_EXT_BLOB: usize = FIELD_ELEMENTS_PER_BLOB as usize * 2;
 
 /// Number of field elements in a cell.
-pub const FIELD_ELEMENTS_PER_CELL: u64 = 64;
+pub const FIELD_ELEMENTS_PER_CELL: usize = 64;
+
+/// The number of bytes in a cell.
+pub const BYTES_PER_CELL: usize = FIELD_ELEMENTS_PER_CELL * FIELD_ELEMENT_BYTES as usize;
 
 /// The number of cells in an extended blob.
-pub const CELLS_PER_EXT_BLOB: u64 = FIELD_ELEMENTS_PER_EXT_BLOB / FIELD_ELEMENTS_PER_CELL;
+pub const CELLS_PER_EXT_BLOB: usize = FIELD_ELEMENTS_PER_EXT_BLOB / FIELD_ELEMENTS_PER_CELL;
+
+/// A wrapper version for EIP-7594 sidecar encoding.
+pub const EIP_7594_WRAPPER_VERSION: u8 = 1;
+
+/// A commitment/proof serialized as 0x-prefixed hex string
+pub type Cell = FixedBytes<BYTES_PER_CELL>;
+
+/// CL-enforced target blobs per block after Fusaka hardfork activation.
+pub const TARGET_BLOBS_PER_BLOCK_FULU: u64 = 48;
+
+/// CL-enforced maximum blobs per block after Fusaka hardfork activation.
+pub const MAX_BLOBS_PER_BLOCK_FULU: u64 = 64;
