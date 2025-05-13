a7e3847660a740838d51485cdb4f31c86a2858d6
Wolfgang Welz <welzwo@gmail.com>
2025-05-13 13:31:00 +0200

    support string and float when deserialiting TTD

--- crates/serde/src/ttd.rs
+++ crates/serde/src/ttd.rs
@@ -9,7 +9,8 @@
 //! This is only enabled for human-readable [`serde`] implementations.
 
 use alloy_primitives::U256;
-use serde::{ser, Deserialize, Deserializer, Serialize, Serializer};
+use serde::{de, ser, Deserialize, Deserializer, Serialize, Serializer};
+use serde_json::Value;
 
 /// Serializes an optional TTD as a JSON number.
 ///
@@ -38,7 +39,10 @@ where
     D: Deserializer<'de>,
 {
     if deserializer.is_human_readable() {
-        Ok(Option::<u128>::deserialize(deserializer)?.map(U256::from))
+        match Option::<Value>::deserialize(deserializer)? {
+            Some(value) => ttd_from_value::<D>(value).map(Some),
+            None => Ok(None),
+        }
     } else {
         Option::<U256>::deserialize(deserializer)
     }
@@ -52,6 +56,55 @@ where
     deserialize(deserializer)
 }
 
+/// Converts the given [serde_json::Value] into a `U256` value for TTD deserialization.
+fn ttd_from_value<'de, D>(val: Value) -> Result<U256, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let val = match val {
+        Value::Number(num) => num,
+        Value::String(raw) => return raw.parse().map_err(<D::Error as de::Error>::custom),
+        _ => return Err(de::Error::custom("TTD must be a number or string")),
+    };
+
+    let num = if let Some(val) = val.as_u64() {
+        U256::from(val)
+    } else if let Some(value) = val.as_f64() {
+        // The ethereum mainnet TTD is 58750000000000000000000, and geth serializes this
+        // without quotes, because that is how golang `big.Int`s marshal in JSON. Numbers
+        // are arbitrary precision in JSON, so this is valid JSON. This number is also
+        // greater than a `u64`.
+        //
+        // Unfortunately, serde_json only supports parsing up to `u64`, resorting to `f64`
+        // once `u64` overflows:
+        // <https://github.com/serde-rs/json/blob/4bc1eaa03a6160593575bc9bc60c94dba4cab1e3/src/de.rs#L1411-L1415>
+        // <https://github.com/serde-rs/json/blob/4bc1eaa03a6160593575bc9bc60c94dba4cab1e3/src/de.rs#L479-L484>
+        // <https://github.com/serde-rs/json/blob/4bc1eaa03a6160593575bc9bc60c94dba4cab1e3/src/de.rs#L102-L108>
+        //
+        // serde_json does have an arbitrary precision feature, but this breaks untagged
+        // enums in serde:
+        // <https://github.com/serde-rs/serde/issues/2230>
+        // <https://github.com/serde-rs/serde/issues/1183>
+        //
+        // To solve this, we use the captured float and return the TTD as a U256 if it's equal.
+        if value == 5.875e22 {
+            U256::from(58750000000000000000000u128)
+        } else {
+            // We could try to convert to a u128 here but there would probably be loss of
+            // precision, so we just return an error.
+            return Err(de::Error::custom(
+                "Deserializing a large non-mainnet TTD is not supported",
+            ));
+        }
+    } else {
+        // must be i64 - negative numbers are not supported
+        return Err(de::Error::custom(
+            "Negative TTD values are invalid and will not be deserialized",
+        ));
+    };
+
+    Ok(num)
+}
 #[cfg(test)]
 mod tests {
     #[cfg(not(feature = "std"))]
@@ -66,18 +119,18 @@ mod tests {
         struct Ttd(#[serde(with = "super")] Option<U256>);
 
         let deserialized: Vec<Ttd> = serde_json::from_str(
-            "[0, 17000000000000000, 58750000000000000000000, 4294967295, 18446744073709551615, 340282366920938463463374607431768211455]",
+            r#"["",0,"0","0x0","58750000000000000000000",58750000000000000000000]"#,
         )
         .unwrap();
         assert_eq!(
             deserialized,
             vec![
                 Ttd(Some(U256::ZERO)),
-                Ttd(Some(U256::from(17000000000000000u64))),
+                Ttd(Some(U256::ZERO)),
+                Ttd(Some(U256::ZERO)),
+                Ttd(Some(U256::ZERO)),
                 Ttd(Some(U256::from(58750000000000000000000u128))),
-                Ttd(Some(U256::from(4294967295u32))),
-                Ttd(Some(U256::from(18446744073709551615u64))),
-                Ttd(Some(U256::from(340282366920938463463374607431768211455u128))),
+                Ttd(Some(U256::from(58750000000000000000000u128))),
             ]
         );
     }
@@ -129,12 +182,22 @@ mod tests {
     }
 
     #[test]
-    fn deserialize_ttd_string() {
+    fn deserialize_ttd_invalid_string() {
         #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
         struct Ttd(#[serde(with = "super")] Option<U256>);
 
-        // strings, even hex, are not allowed
-        let result: Result<Ttd, _> = serde_json::from_value(json!("0x0"));
+        // Invalid string that cannot be parsed into U256
+        let result: Result<Ttd, _> = serde_json::from_value(json!("invalid_string"));
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn deserialize_ttd_large_non_mainnet() {
+        #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+        struct Ttd(#[serde(with = "super")] Option<U256>);
+
+        // Test for a large number not equal to 5.875e22, which should result in an error
+        let result: Result<Ttd, _> = serde_json::from_value(json!(6.0e22));
         assert!(result.is_err());
     }
 
@@ -146,5 +209,15 @@ mod tests {
         // Test for a negative number which should not be allowed
         let result: Result<Ttd, _> = serde_json::from_value(json!(-1));
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn deserialize_ttd_as_string() {
+        #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+        struct Ttd(#[serde(with = "super")] Option<U256>);
+
+        // Test for valid TTD as a string
+        let deserialized: Ttd = serde_json::from_value(json!("0x12345")).unwrap();
+        assert_eq!(deserialized, Ttd(Some(U256::from(0x12345))));
     }
 }
