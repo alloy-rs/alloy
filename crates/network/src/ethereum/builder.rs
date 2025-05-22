ec84e342b3c9cd46c52e669fec5fbd9c121f3d92
fantasyup <59591096+fantasyup@users.noreply.github.com>
2025-05-22 13:56:45 -0400

    feat: add option to always set input+data in MulticallBuilder (#2491)
    * feat: add option to always set input+data in MulticallBuilder
    
    * address comments
    
    * touchups
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/network/src/ethereum/builder.rs
+++ crates/network/src/ethereum/builder.rs
@@ -4,7 +4,7 @@ use crate::{
 };
 use alloy_consensus::{TxType, TypedTransaction};
 use alloy_primitives::{Address, Bytes, ChainId, TxKind, U256};
-use alloy_rpc_types_eth::{request::TransactionRequest, AccessList};
+use alloy_rpc_types_eth::{request::TransactionRequest, AccessList, TransactionInputKind};
 
 impl TransactionBuilder<Ethereum> for TransactionRequest {
     fn chain_id(&self) -> Option<ChainId> {
@@ -163,6 +163,18 @@ impl TransactionBuilder<Ethereum> for TransactionRequest {
         wallet: &W,
     ) -> Result<<Ethereum as Network>::TxEnvelope, TransactionBuilderError<Ethereum>> {
         Ok(wallet.sign_request(self).await?)
+    }
+
+    fn set_input_kind<T: Into<Bytes>>(&mut self, input: T, kind: TransactionInputKind) {
+        match kind {
+            TransactionInputKind::Input => self.input.input = Some(input.into()),
+            TransactionInputKind::Data => self.input.data = Some(input.into()),
+            TransactionInputKind::Both => {
+                let bytes = input.into();
+                self.input.input = Some(bytes.clone());
+                self.input.data = Some(bytes);
+            }
+        }
     }
 }
 
