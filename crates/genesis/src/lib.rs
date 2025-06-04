f31de86e778eda08f47a1e55cde37339ddddd5ea
Roman Krasiuk <rokrassyuk@gmail.com>
2025-06-04 11:40:09 +0200

    fix no_std

--- crates/genesis/src/lib.rs
+++ crates/genesis/src/lib.rs
@@ -11,7 +11,7 @@
 
 extern crate alloc;
 
-use alloc::{collections::BTreeMap, string::String};
+use alloc::{collections::BTreeMap, string::String, vec::Vec};
 use alloy_eips::{eip7840::BlobParams, BlobScheduleBlobParams};
 use alloy_primitives::{keccak256, Address, Bytes, B256, U256};
 use alloy_serde::{storage::deserialize_storage_map, ttd::deserialize_json_ttd_opt, OtherFields};
