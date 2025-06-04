b5301650f6fdf2aa939b7051a5f7bc6383a260a2
Roman Krasiuk <rokrassyuk@gmail.com>
2025-06-04 11:46:56 +0200

    feat: EIP-7918

--- crates/consensus/src/block/header.rs
+++ crates/consensus/src/block/header.rs
@@ -228,7 +228,11 @@ impl Header {
     ///
     /// Returns a `None` if no excess blob gas is set, no EIP-4844 support
     pub fn next_block_excess_blob_gas(&self, blob_params: BlobParams) -> Option<u64> {
-        Some(blob_params.next_block_excess_blob_gas(self.excess_blob_gas?, self.blob_gas_used?))
+        Some(blob_params.next_block_excess_blob_gas(
+            self.excess_blob_gas?,
+            self.blob_gas_used?,
+            self.base_fee_per_gas?,
+        ))
     }
 
     /// Calculate a heuristic for the in-memory size of the [Header].
@@ -636,7 +640,11 @@ pub trait BlockHeader {
     ///
     /// Returns a `None` if no excess blob gas is set, no EIP-4844 support
     fn next_block_excess_blob_gas(&self, blob_params: BlobParams) -> Option<u64> {
-        Some(blob_params.next_block_excess_blob_gas(self.excess_blob_gas()?, self.blob_gas_used()?))
+        Some(blob_params.next_block_excess_blob_gas(
+            self.excess_blob_gas()?,
+            self.blob_gas_used()?,
+            self.base_fee_per_gas()?,
+        ))
     }
 
     /// Convenience function for [`Self::next_block_excess_blob_gas`] with an optional
