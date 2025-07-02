e1fb9d9d2833e1302cc967f0cff498b94b3a6802
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-07-02 18:31:19 +0530

    feat(`network`): use `FullSigner` in `EthereumWallet` to sign data (#2523)
    * feat(`network`): use `FullSigner` in `EthereumWallet`
    
    * feat(`network`): EthWallet.sign_hash_with
    
    * clippy
    
    * remove signer_mut
    
    * fix

--- crates/network/src/ethereum/mod.rs
+++ crates/network/src/ethereum/mod.rs
@@ -3,7 +3,7 @@ use crate::Network;
 mod builder;
 
 mod wallet;
-pub use wallet::{EthereumWallet, IntoWallet};
+pub use wallet::{ArcFullSigner, EthereumWallet, IntoWallet};
 
 /// Types for a mainnet-like Ethereum network.
 #[derive(Clone, Copy, Debug)]
