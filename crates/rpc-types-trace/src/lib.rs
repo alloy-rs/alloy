14baa5292e5ae54e07d319968ee408f5e5077098
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-09-28 04:09:19 +0200

    chore: remove feature(doc_auto_cfg)

--- crates/rpc-types-trace/src/lib.rs
+++ crates/rpc-types-trace/src/lib.rs
@@ -5,7 +5,7 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![deny(unused_must_use, rust_2018_idioms)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 pub mod common;
 pub mod filter;
