1b843a056f8396ee99a12edf2ba8b6ae711bc4be
leruaa <aurelien.catinon@gmail.com>
2025-11-03 05:26:31 -0800

    fix: BlobParams bincode deserialization

--- crates/eips/src/eip7840.rs
+++ crates/eips/src/eip7840.rs
@@ -181,7 +181,6 @@ mod serde_impl {
         max_blob_count: u64,
         #[serde(rename = "target")]
         target_blob_count: u64,
-        #[serde(skip_serializing)]
         min_blob_fee: Option<u128>,
     }
 
