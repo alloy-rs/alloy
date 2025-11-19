0441bffe6fb1c80d174c8fbc240bcf91188446e5
0xLogicalx <danilokuniman@gmail.com>
2025-11-19 21:41:19 +0100

    Update web3_signer.rs

--- crates/provider/src/provider/web3_signer.rs
+++ crates/provider/src/provider/web3_signer.rs
@@ -71,10 +71,15 @@ impl<P: Provider<N> + Clone, N: Network> Web3Signer<P, N> {
 
 #[cfg(test)]
 mod tests {
+    #[cfg(not(windows))]
     use super::*;
+    #[cfg(not(windows))]
     use crate::{ext::test::async_ci_only, Provider, ProviderBuilder};
+    #[cfg(not(windows))]
     use alloy_consensus::{transaction::SignerRecoverable, TxEnvelope};
+    #[cfg(not(windows))]
     use alloy_node_bindings::{utils::run_with_tempdir, Reth};
+    #[cfg(not(windows))]
     use alloy_primitives::{Address, U256};
 
     #[tokio::test]
