a2ff011164fd8fdfbd82b6efd47a594eac1bb715
Matthias Seitz <matthias.seitz@outlook.de>
2025-12-01 16:52:46 +0100

    Update crates/network-primitives/src/tx_meta.rs
    Co-authored-by: onbjerg <onbjerg@users.noreply.github.com>

--- crates/network-primitives/src/tx_meta.rs
+++ crates/network-primitives/src/tx_meta.rs
@@ -1,7 +1,7 @@
 use alloy_primitives::B256;
 
 /// Additional fields in the context of a block that contains this _mined_ transaction.
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
+#[derive(Debug, Clone, Default, Eq, PartialEq)]
 pub struct MinedTransactionInfo {
     /// The hash of the block.
     pub block_hash: B256,
