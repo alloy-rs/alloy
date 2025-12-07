fad7ba953baef81578c95366329c3797180ce521
0xcharry <idea.sermons.5p@icloud.com>
2025-12-07 15:30:41 +0100

    Update native.rs

--- crates/transport-ws/src/native.rs
+++ crates/transport-ws/src/native.rs
@@ -93,13 +93,15 @@ impl WsConnect {
         self.retry_interval = retry_interval;
         self
     }
-}
+    fn client_request(
+        &self,
+    ) -> tungstenite::Result<tungstenite::handshake::client::Request> {
+        let mut request: http::Request<()> =
+            self.url.clone().into_client_request()?;
 
-impl IntoClientRequest for WsConnect {
-    fn into_client_request(self) -> tungstenite::Result<tungstenite::handshake::client::Request> {
-        let mut request: http::Request<()> = self.url.into_client_request()?;
-        if let Some(auth) = self.auth {
-            let mut auth_value = http::HeaderValue::from_str(&auth.to_string())?;
+        if let Some(auth) = &self.auth {
+            let mut auth_value =
+                http::HeaderValue::from_str(&auth.to_string())?;
             auth_value.set_sensitive(true);
 
             request.headers_mut().insert(http::header::AUTHORIZATION, auth_value);
@@ -109,14 +111,19 @@ impl IntoClientRequest for WsConnect {
     }
 }
 
+impl IntoClientRequest for WsConnect {
+    fn into_client_request(self) -> tungstenite::Result<tungstenite::handshake::client::Request> {  
+        self.client_request()
+    }
+}
+
 impl PubSubConnect for WsConnect {
     fn is_local(&self) -> bool {
         alloy_transport::utils::guess_local_url(&self.url)
     }
 
     async fn connect(&self) -> TransportResult<alloy_pubsub::ConnectionHandle> {
-        let request = self.clone().into_client_request();
-        let req = request.map_err(TransportErrorKind::custom)?;
+        let req = self.client_request().map_err(TransportErrorKind::custom)?;
         let (socket, _) = tokio_tungstenite::connect_async_with_config(req, self.config, false)
             .await
             .map_err(TransportErrorKind::custom)?;
