2c6f58e45f865d0cf3731d8a94ab83e244ffdf2b
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2025-12-11 12:45:44 +0100

    chore: fmt

--- crates/consensus/src/transaction/envelope.rs
+++ crates/consensus/src/transaction/envelope.rs
@@ -1065,8 +1065,8 @@ mod tests {
     use alloy_eips::{
         eip2930::{AccessList, AccessListItem},
         eip4844::BlobTransactionSidecar,
+        eip7594::BlobTransactionSidecarVariant,
         eip7702::Authorization,
-        eip7594::BlobTransactionSidecarVariant
     };
     #[allow(unused_imports)]
     use alloy_primitives::{b256, Bytes, TxKind};
@@ -1548,25 +1548,27 @@ mod tests {
         });
         test_serde_roundtrip(tx);
 
-        let tx = TxEip4844Variant::<BlobTransactionSidecarVariant>::TxEip4844WithSidecar(TxEip4844WithSidecar {
-            tx: TxEip4844 {
-                chain_id: 1,
-                nonce: 100,
-                max_fee_per_gas: 50_000_000_000,
-                max_priority_fee_per_gas: 1_000_000_000_000,
-                gas_limit: 1_000_000,
-                to: Address::random(),
-                value: U256::from(10e18),
-                input: Bytes::new(),
-                access_list: AccessList(vec![AccessListItem {
-                    address: Address::random(),
-                    storage_keys: vec![B256::random()],
-                }]),
-                blob_versioned_hashes: vec![B256::random()],
-                max_fee_per_blob_gas: 0,
+        let tx = TxEip4844Variant::<BlobTransactionSidecarVariant>::TxEip4844WithSidecar(
+            TxEip4844WithSidecar {
+                tx: TxEip4844 {
+                    chain_id: 1,
+                    nonce: 100,
+                    max_fee_per_gas: 50_000_000_000,
+                    max_priority_fee_per_gas: 1_000_000_000_000,
+                    gas_limit: 1_000_000,
+                    to: Address::random(),
+                    value: U256::from(10e18),
+                    input: Bytes::new(),
+                    access_list: AccessList(vec![AccessListItem {
+                        address: Address::random(),
+                        storage_keys: vec![B256::random()],
+                    }]),
+                    blob_versioned_hashes: vec![B256::random()],
+                    max_fee_per_blob_gas: 0,
+                },
+                sidecar: Default::default(),
             },
-            sidecar: Default::default(),
-        });
+        );
         test_serde_roundtrip(tx);
     }
 
