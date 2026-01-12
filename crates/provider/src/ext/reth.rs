2636018ab0b4a2b7dd24543a722f8bfb5a445ca8
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2026-01-12 11:37:27 +0530

    chore: RethProviderExt impl

--- crates/provider/src/ext/reth.rs
+++ crates/provider/src/ext/reth.rs
@@ -1,18 +1,21 @@
+//! Reth-specific provider extensions.
 #[cfg(feature = "pubsub")]
 use crate::GetSubscription;
-use alloy_json_rpc::RpcError;
+use crate::Provider;
+use alloy_network::Network;
 use alloy_primitives::{map::HashMap, Address, U256};
 use alloy_rpc_types_eth::BlockId;
+use alloy_transport::TransportResult;
 
 /// Reth API namespace for reth-specific methods
 #[cfg_attr(target_family = "wasm", async_trait::async_trait(?Send))]
 #[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
-pub trait RethProviderExt {
+pub trait RethProviderExt<N: Network>: Send + Sync {
     /// Returns all ETH balance changes in a block
     async fn reth_get_balance_changes_in_block(
         &self,
         block_id: BlockId,
-    ) -> Result<HashMap<Address, U256>, RpcError<()>>;
+    ) -> TransportResult<HashMap<Address, U256>>;
 
     /// Subscribe to json `ChainNotifications`
     #[cfg(feature = "pubsub")]
@@ -27,4 +30,33 @@ pub trait RethProviderExt {
     async fn reth_subscribe_persisted_block(
         &self,
     ) -> GetSubscription<alloy_rpc_client::NoParams, serde_json::Value>;
+}
+
+#[cfg_attr(target_family = "wasm", async_trait::async_trait(?Send))]
+#[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
+impl<N, P> RethProviderExt<N> for P
+where
+    N: Network,
+    P: Provider<N>,
+{
+    async fn reth_get_balance_changes_in_block(
+        &self,
+        block_id: BlockId,
+    ) -> TransportResult<HashMap<Address, U256>> {
+        self.client().request("reth_getBalanceChangesInBlock", (block_id,)).await
+    }
+
+    #[cfg(feature = "pubsub")]
+    async fn reth_subscribe_chain_notifications(
+        &self,
+    ) -> GetSubscription<alloy_rpc_client::NoParams, serde_json::Value> {
+        self.client().subscribe_to("reth_subscribeChainNotifications")
+    }
+
+    #[cfg(feature = "pubsub")]
+    async fn reth_subscribe_persisted_block(
+        &self,
+    ) -> GetSubscription<alloy_rpc_client::NoParams, serde_json::Value> {
+        self.client().subscribe_to("reth_subscribePersistedBlock")
+    }
 }
