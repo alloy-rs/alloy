42266dd1aa0ea9c78df7db28146d91ef81de9e01
Antony Denyer <email@antonydenyer.co.uk>
2026-01-22 13:50:54 +0000

    Make error optional
    - switch type to Bytes to be inline with eth_call

--- crates/rpc-types-eth/src/simulate.rs
+++ crates/rpc-types-eth/src/simulate.rs
@@ -192,9 +192,9 @@ pub struct SimulateError {
     pub code: i32,
     /// Message error
     pub message: String,
-    /// Data for the error e.g revert reason
-    #[cfg_attr(feature = "serde", serde(default))]
-    pub data: String,
+    /// Data for the error, e.g. revert reason.
+    #[cfg_attr(feature = "serde", serde(default, skip_serializing_if = "Option::is_none"))]
+    pub data: Option<Bytes>,
 }
 
 impl SimulateError {
@@ -220,7 +220,7 @@ mod tests {
             "message": "Execution reverted"
         });
         let err: SimulateError = serde_json::from_value(error_json).unwrap();
-        assert_eq!(err.data, "");
+        assert_eq!(err.data, None);
     }
 
     #[test]
@@ -232,7 +232,7 @@ mod tests {
             "data": "0xcabedea8"
         });
         let err: SimulateError = serde_json::from_value(error_json).unwrap();
-        assert_eq!(err.data, "0xcabedea8");
+        assert_eq!(err.data, Some("0xcabedea8".into()));
     }
 
     #[test]
