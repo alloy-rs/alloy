60d290da76afcb2423a7342261810edac2674e2b
Matthias Seitz <matthias.seitz@outlook.de>
2026-03-08 09:42:43 +0100

    feat(alloy): support no_std for the meta crate
    The `std` feature and its propagation to sub-crates was already in place,
    but the `no_std` attribute was missing from `lib.rs`.

--- crates/alloy/src/lib.rs
+++ crates/alloy/src/lib.rs
@@ -5,6 +5,7 @@
 )]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 /* --------------------------------------- Core re-exports -------------------------------------- */
 
