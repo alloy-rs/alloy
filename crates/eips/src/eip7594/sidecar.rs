192d7c8c8e7e847d92b527b2ecdb9c665981bbbf
Matthias Seitz <matthias.seitz@outlook.de>
2026-08-12 13:50:15 +0200

    chore(eips): remove redundant format import (#4134)

--- crates/eips/src/eip7594/sidecar.rs
+++ crates/eips/src/eip7594/sidecar.rs
@@ -5,7 +5,7 @@ use crate::{
     },
     eip7594::{CELLS_PER_EXT_BLOB, EIP_7594_WRAPPER_VERSION},
 };
-use alloc::{boxed::Box, format, vec::Vec};
+use alloc::{boxed::Box, vec::Vec};
 use alloy_primitives::{B128, B256};
 use alloy_rlp::{BufMut, Decodable, Encodable, Header};
 
